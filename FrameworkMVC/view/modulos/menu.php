b5c01e2390240a0f9c425442a1067adee2e30f92
mAYCOL <maycol@masoft.net>
2016-05-10 16:05:20 -0500

    CAMBIOS EN EL MENU

--- FrameworkMVC/view/modulos/menu.php
+++ FrameworkMVC/view/modulos/menu.php
@@ -25,98 +25,11 @@
 			</li>
 			<li><a href="index.php?controller=PermisosRoles&action=index">Permisos Roles</a>
 			</li>
-		    <li><a href="index.php?controller=RegistroCartonDocumentos&action=index">Registro de Cartones</a>
-			</li>
-		
-	          
+		    
           </ul>
         </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-file" ><?php echo " Gestión Documental" ;?> </span> <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          <li><a href="index.php?controller=Categorias&action=index">Categorias</a>
-		    </li>
-			<li><a href="index.php?controller=SubCategorias&action=index">Subcategorias</a>
-			</li>
-			<li><a href="index.php?controller=TiposDocumentos&action=index">Tipos de Documentos</a>
-			</li>
-			<li><a href="index.php?controller=CartonDocumentos&action=index">Cartones de Documentos</a>
-			</li>
-			<li><a href="index.php?controller=ClienteProveedor&action=index">Clientes / Proveedores</a>
-			</li>
-			<li><a href="index.php?controller=Soat&action=index">SOAT</a>
-			</li>
-			<li role="separator" class="divider"></li>
-			<li><a href="index.php?controller=Documentos&action=index">Busqueda de Documentos</a>
-			</li>
-			<li><a href="index.php?controller=DocumentosClienteProveedor&action=index">Busqueda Categorias + Cliente / Proveedor</a>
-			</li>
-			<li><a href="index.php?controller=DocumentosTipoDocumentos&action=index">Busqueda Categorias + Tipo Documentos</a>
-			</li>
-	        <li><a href="index.php?controller=DocumentosCartonDocumentos&action=index">Busqueda Categorias + Carton Documentos</a>
-			</li>
-			<li><a href="index.php?controller=DocumentosNumeroPoliza&action=index">Busqueda Categorias + Numero Poliza</a>
-			</li>
-			<li><a href="index.php?controller=DocumentosCierreSoat&action=index">Busqueda Categorias + Cierre SOAT</a>
-			</li>
-			
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-print	" ><?php echo " Informes" ;?> </span> <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-        	<li><a href="index.php?controller=Categorias&action=ReporteTotal" target="blank">Documentos por Categorías</a>
-			</li>
-			<li><a href="index.php?controller=SubCategorias&action=ReporteTotal" target="blank">Documentos por SubCategorías</a>
-			</li>
-			<li><a href="index.php?controller=Documentos&action=BuscaxCarton" >Documentos por Cartón</a>
-			</li>
-			<li role="separator" class="divider"></li>
-			
-			<li><a href="index.php?controller=CartonDocumentos&action=ReporteTotal" >Listado de Cartones Registrados</a>
-			</li>
-			
-	        <li role="separator" class="divider"></li>
-           <li><a href="index.php?controller=ErroresImportacion&action=index" target="blank">Errores de Importación</a>
-		    </li>
-		
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-modal-window" ><?php echo " Utilitarios" ;?> </span> <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="index.php?controller=CartonImpreso&action=index">Impresión de Etiqueta de Cartón</a>
-			</li>
-			<li><a href="index.php?controller=Documentos&action=ActualizarDocumentos">Actualizar Documentos</a>
-			</li>
-			
-		  </ul>
-        </li>
-        
-        
-      </ul>
-      <form class="navbar-form navbar-right" role="search" action="<?php echo $helper->url("Documentos","Buscador");	
-				?>"  method="post" >
-  		      		      
+       
       
-        <div class="form-group">
-          
-          <input type="text" class="form-control" name="contenido_busqueda" id="criterio_busqueda" placeholder="texto a buscar">
-          
-          <select id="criterio_busqueda" name="criterio_busqueda" class="form-control">
-				<option value="0"  > --TODOS--</option>
-				<option value="1"  >Ruc Cliente/Proveedor</option>
-				<option value="2"  >Nombre Cliente/Proveedor</option>
-				<option value="3"  >Número Carton</option>
-				<option value="4"  >Número Poliza</option>
-				<option value="5"  >Ramo Póliza</option>
-				<option value="6"  >Ciudad Emisión</option>
-				<option value="7"  >Tipo de Documento</option>
-		 </select>
-						   		
-        </div>
-        <button type="submit"  name="btn_buscar" class="btn btn-default"><span class="glyphicon glyphicon-search	" ><?php echo " Buscar" ;?> </span></button>
-      </form>
       
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
