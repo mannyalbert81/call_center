6f12f8281ded39fdc6da9761239393e6847e5175
Danny <danny@masoft.net>
2016-05-10 10:19:44 -0500

    hgghgf

--- FrameworkMVC/view/modulos/head.php
+++ FrameworkMVC/view/modulos/head.php
@@ -40,7 +40,7 @@
  		<p> <strong> <?php echo CLIENTE?>  </strong>  </p>
  		</div>	
 		<?php  
-		//bmmnb
+		//bmmn
 			 $status = session_status();
 			 if  (isset( $_SESSION['nombre_usuario'] ))  {  
 		?>
