e724f5a2d668fb4f2cd1bc19532f4d15b7f77e83
mAYCOL <maycol@masoft.net>
2016-05-11 15:16:25 -0500

    cambios

--- FrameworkMVC/view/modulos/head.php
+++ FrameworkMVC/view/modulos/head.php
@@ -18,7 +18,8 @@
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 	    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-		<link rel="stylesheet" href="/resources/demos/style.css">
+		
+		<link rel="stylesheet" href="view/css/estilos.css">
 		<link rel="stylesheet" href="http://jqueryvalidation.org/files/demo/site-demos.css">
         <script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>
         <script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
