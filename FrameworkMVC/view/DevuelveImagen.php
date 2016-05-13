0f36fab25ef065022e09203ed1eaf55c16f88df7
ManuelAlberto <manuel@masoft.net>
2016-05-13 15:57:44 -0500

    conexion

--- FrameworkMVC/view/DevuelveImagen.php
+++ FrameworkMVC/view/DevuelveImagen.php
@@ -26,6 +26,7 @@ if (isset ($_GET["id_valor"]))
 }
 $image = "";
 $conn  = pg_connect("user=postgres port=5432 password=.Romina.2012 dbname=coactiva host=192.168.100.3");
+//$conn  = pg_connect("user=postgres port=5433 password=.Romina.2012 dbname=coactiva host=186.4.241.148");
 if(!$conn)
 {
 	echo  "No se pudo conectar";
