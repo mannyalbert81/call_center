a310394003cadd9ca513cee5a130a3fdac46a5b5
ManuelAlberto <manuel@masoft.net>
2016-05-20 17:29:57 -0500

    hola

--- FrameworkMVC/view/modulos/footer.php
+++ FrameworkMVC/view/modulos/footer.php
@@ -1,6 +1,6 @@
  <div class="navbar navbar-fixed-bottom"  >
        <footer  style="background:#000000; text-align: center; color: #FFFFFF;">
-            <hr/  >
+            <hr/>
           Coactivas  - AllCoercive 2016 - <a href="http://www.masoft.net">www.masoft.net</a> - Copyright &copy; <?php echo  date("Y"); ?>
         </footer>
   </div>    
