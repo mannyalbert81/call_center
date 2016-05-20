a5d542872aff12ff232e0bcc3c5dec04902e7ed1
Danny <danny@masoft.net>
2016-05-20 15:15:25 -0500

    tipohonorarios cambios

---
+++ FrameworkMVC/controller/TipoHonorariosController.php
@@ -0,0 +1,251 @@
+<?php
+
+class TipoHonorariosController extends ControladorBase{
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+
+
+	public function index(){
+		
+		session_start();
+			
+		$resultEdit = "";
+		
+		$tipoHonorarios=new TipoHonorariosModel();
+	
+		$resultSet=$tipoHonorarios->getAll("id_tipo_honorarios");
+		
+		if (isset(  $_SESSION['usuario_usuarios']) )
+		{
+			
+
+			$nombre_controladores = "TipoHonorarios";
+			$id_rol= $_SESSION['id_rol'];
+			$resultPer = $tipoHonorarios->getPermisosVer("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+			
+			if (!empty($resultPer))
+			{
+										
+				$resultPEdit = $tipoHonorarios->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+					
+				
+				if (!empty($resultPEdit)  )
+				{
+						if (isset ($_GET["id_ltcredito"]) )
+					{
+					
+						$_id_lotesTituloCredito = $_GET["id_ltcredito"];
+						$columnas = " id_lotes_titulos_credito, nombre_lotes_titulos_credito";
+						$tablas   = "lotes_titulos_credito";
+						$where    = "id_lotes_titulos_credito = '$_id_lotesTituloCredito' "; 
+						$id       = "id_lotes_titulos_credito";
+							
+						
+						$resultEdit = $tipoHonorarios->getCondiciones($columnas ,$tablas ,$where, $id);
+						
+						$traza=new TrazasModel();
+						$_nombre_controlador = "Lotes Titulo Credito";
+						$_accion_trazas  = "Editar";
+						$_parametros_trazas = $_id_lotesTituloCredito;
+						$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);;
+						
+					
+					}
+					else
+					{
+						$this->view("Error",array(
+								"resultado"=>"No tiene Permisos de Editar Lotes Titulo Credito"
+					
+						));
+					
+					
+					}
+					
+				}
+				
+				
+		
+				
+				$this->view("TipoHonorarios",array(
+						"resultSet"=>$resultSet, "resultEdit" =>$resultEdit
+			
+				));
+		
+				
+				
+			}
+			else
+			{
+				$this->view("Error",array(
+						"resultado"=>"No tiene Permisos de Acceso a Lotes Titulo Credito"
+				
+				));
+				
+				exit();	
+			}
+				
+		}
+		else 
+		{
+				$this->view("ErrorSesion",array(
+						"resultSet"=>"Debe Iniciar Session"
+			
+				));
+		
+		}
+	
+	}
+	
+	public function InsertaTipoHonorarios(){
+			
+		session_start();
+
+		$permisos_rol=new PermisosRolesModel();
+
+		$tipoHonorarios=new TipoHonorariosModel();
+
+
+		$nombre_controladores = "TipoHonorarios";
+		$id_rol= $_SESSION['id_rol'];
+
+		
+		$resultPer = $permisos_rol->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+		
+		if (!empty($resultPer))
+		{
+		
+			$resultado = null;
+			
+		
+			
+			
+			if (isset ($_POST["nombre_ltcredito"]) )
+				
+			{
+				
+				
+				
+				$_nombre_ltCredito = $_POST["nombre_ltcredito"];
+				
+				
+				if(isset($_POST["id_ltcredito"])) 
+				{
+					
+					$_id_ltCredito = $_POST["id_ltcredito"];
+					
+					$colval = " nombre_lotes_titulos_credito = '$_nombre_ltCredito'   ";
+					$tabla = "lotes_titulos_credito";
+					$where = "id_lotes_titulos_credito = '$_id_ltCredito'    ";
+					
+					$resultado=$ltCredito->UpdateBy($colval, $tabla, $where);
+					
+				}else {
+					
+			
+				
+				$funcion = "ins_lotes_titulo_credito";
+				
+				$parametros = " '$_nombre_ltCredito'  ";
+					
+				$ltCredito->setFuncion($funcion);
+		
+				$ltCredito->setParametros($parametros);
+		
+		
+				$resultado=$ltCredito->Insert();
+				
+				$traza=new TrazasModel();
+				$_nombre_controlador = "Lotes Titulo Credito";
+				$_accion_trazas  = "Guardar";
+				$_parametros_trazas = $_nombre_ltCredito;
+				$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);
+				
+			 }
+		
+			}
+			$this->redirect("LotesTituloCredito", "index");
+
+		}
+		else
+		{
+			$this->view("Error",array(
+					
+					"resultado"=>"No tiene Permisos de Insertar Lotes Titulo Credito"
+		
+			));
+		
+		
+		}
+		
+	}
+	
+	public function borrarId()
+	{
+
+		session_start();
+		
+		$permisos_rol=new PermisosRolesModel();
+		$nombre_controladores = "LotesTituloCredito";
+		$id_rol= $_SESSION['id_rol'];
+		$resultPer = $permisos_rol->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+			
+		if (!empty($resultPer))
+		{
+			if(isset($_GET["id_ltcredito"]))
+			{
+				$id_ltCredito=(int)$_GET["id_ltcredito"];
+			
+				$ltCredito = new LotesTituloCreditoModel();
+				$ltCredito->deleteBy("id_lotes_titulos_credito",$id_ltCredito);
+				
+				$traza=new TrazasModel();
+				$_nombre_controlador = "Lotes Titulo Credito";
+				$_accion_trazas  = "Borrar";
+				$_parametros_trazas = $id_ltCredito;
+				$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);
+				
+			}
+			
+			$this->redirect("LotesTituloCredito", "index");
+			
+			
+		}
+		else
+		{
+			$this->view("Error",array(
+				"resultado"=>"No tiene Permisos de Borrar Lotes Titulo Credito"
+			
+			));
+		}
+				
+	}
+	
+	
+	public function Reporte(){
+	
+		//Creamos el objeto usuario
+		$roles=new RolesModel();
+		//Conseguimos todos los usuarios
+		
+	
+	
+		session_start();
+	
+	
+		if (isset(  $_SESSION['usuario']) )
+		{
+			$resultRep = $roles->getByPDF("id_rol, nombre_rol", " nombre_rol != '' ");
+			$this->report("Roles",array(	"resultRep"=>$resultRep));
+	
+		}
+					
+	
+	}
+	
+	
+	
+}
+?>
---
+++ FrameworkMVC/view/TipoHonorariosView.php
@@ -0,0 +1,193 @@
+<!DOCTYPE HTML>
+<html lang="es">
+
+      <head>
+      
+        <meta charset="utf-8"/>
+        <title>Tipo Honorarios - aDocument 2015</title>
+        
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		  			   
+          <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	      <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		
+		<link rel="stylesheet" href="http://jqueryvalidation.org/files/demo/site-demos.css">
+        <script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>
+        <script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+ 		
+ 		<script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
+		
+		<script>
+		    webshims.setOptions('forms-ext', {types: 'date'});
+			webshims.polyfill('forms forms-ext');
+		</script>
+		
+        
+        
+       <style>
+            input{
+                margin-top:5px;
+                margin-bottom:5px;
+            }
+            .right{
+                float:right;
+            }
+                
+            
+        </style>
+       
+    <script >
+    $(document).ready(function(){
+
+        $("#Guardar").click(function(){
+
+        	var ltCredito = $("#nombre_ltcredito").val();
+
+			if (ltCredito == "")
+	    	{
+				$("#mensaje_nombre_ltcredito").text("INGRESE INFORMACION REQUERIDA");
+	    		$("#mensaje_nombre_ltcredito").fadeIn("slow"); //Muestra mensaje de error
+	            return false;
+		    }
+	    	else 
+	    	{
+	    		$("#mensaje_nombre_ltcredito").fadeOut("slow"); //Muestra mensaje de error
+	    		
+	            
+			}    
+
+			
+            });
+
+        $( "#nombre_ltcredito" ).focus(function() {
+			  $("#mensaje_nombre_ltcredito").fadeOut("slow");
+		    });
+
+		
+
+        });
+	
+	</script>
+    </head>
+    <body style="background-color: #d9e3e4;">
+    
+       <?php include("view/modulos/head.php"); ?>
+       
+       <?php include("view/modulos/menu.php"); ?>
+       
+       
+       
+       <?php
+       
+       
+       
+		   
+		?>
+ 
+  
+  <div class="container">
+  
+  <div class="row" style="background-color: #ffffff;">
+  
+       <!-- empieza el form --> 
+       
+      <form action="<?php echo $helper->url("TipoHonorarios","InsertaTipoHonorarios"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-6">
+            
+         
+        	    <h4 style="color:#ec971f;">Insertar Lotes Titulo Credito</h4>
+            	<hr/>
+            	
+		   		
+            
+          <?php if ($resultEdit !="" ) { foreach($resultEdit as $resEdit) {?>
+            
+            
+            
+        
+			   
+			   <div class="row">
+		       <div class="col-xs-12 col-md-12">
+			  	<p  class="formulario-subtitulo" >Nombre Tipo Honorario</p>
+			  	<input type="text"  name="nombre_tipohonorario" id="nombre_tipohonorario" value="<?php echo $resEdit->nombre_lotes_titulos_credito; ?>" class="form-control"/> 
+			  	<input type="hidden"  name="id_tipoHonorario"  value="<?php echo $resEdit->id_lotes_titulos_credito; ?>" class="form-control"/> 
+			    <div id="mensaje_tipoHonorario" class="errores"></div>
+			  </div>
+			   </div>
+		    
+		     <?php } } else {?>
+		    
+			   <div class="row">
+		       <div class="col-xs-6 col-md-6">
+			  	<p  class="formulario-subtitulo" >Nombre Tipo Honorario</p>
+			  	<input type="text"  name="nombre_tipohonorario" id="nombre_tipohonorario" value="" class="form-control"/> 
+			    <div id="mensaje_tipoHonorario" class="errores"></div>
+			  </div>
+			 </div>
+
+		    <hr>
+		    
+		   
+               	
+		     <?php } ?>
+		     
+		     
+		       <div class="row">
+			  <div class="col-xs-12 col-md-6" style="text-align: center;" >
+			  	<input type="submit" id="Guardar" name="Guardar" value="Guardar" class="btn btn-success"/>
+			  </div>
+			</div>     
+               
+		
+		 <hr>
+          
+       </form>
+       <!-- termina el form --> 
+       
+        <div class="col-lg-6">
+            <h4 style="color:#ec971f;">Lista de Lotes Titulo Credito</h4>
+            <hr/>
+        </div>
+        <section class="col-lg-6 usuario" style="height:400px;overflow-y:scroll;">
+        <table class="table table-hover ">
+	         <tr >
+	    		<th style="color:#456789;font-size:80%;"><b>Id</b></th>
+	    		<th style="color:#456789;font-size:80%;">Nombre</th>
+	    		
+	    		<th></th>
+	    		<th></th>
+	  		</tr>
+            
+	            <?php if (!empty($resultSet)) {  foreach($resultSet as $res) {?>
+	        		<tr>
+	                   <td style="color:#000000;font-size:80%;"> <?php echo $res->id_lotes_titulos_credito; ?></td>
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_lotes_titulos_credito; ?>     </td> 
+		              
+		           	   <td>
+			           		<div class="right">
+			                    <a href="<?php echo $helper->url("TipoHonorarios","index"); ?>&id_tipoHonorario=<?php echo $res->id_lotes_titulos_credito; ?>" class="btn btn-warning" style="font-size:65%;">Editar</a>
+			                </div>
+			            
+			             </td>
+			             <td>   
+			                	<div class="right">
+			                    <a href="<?php echo $helper->url("TipoHonorarios","borrarId"); ?>&id_tipoHonorario=<?php echo $res->id_lotes_titulos_credito; ?>" class="btn btn-danger" style="font-size:65%;">Borrar</a>
+			                </div>
+			                <hr/>
+		               </td>
+		    		</tr>
+		        <?php } } ?>
+            
+            <?php 
+            
+            //echo "<script type='text/javascript'> alert('Hola')  ;</script>";
+            
+            ?>
+            
+       	</table>     
+      </section>
+      </div>
+      </div>
+   
+     </body>  
+    </html>   
