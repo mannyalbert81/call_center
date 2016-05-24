4ff0095861ae467357fdd2efc945950d1e1dbb57
steven <steven@masoft.net>
2016-05-24 17:28:41 -0500

    PASANTE

---
+++ FrameworkMVC/controller/VehiculosEmbargadosController.php
@@ -0,0 +1,314 @@
+<?php
+
+class VehiculosEmbargadosController extends ControladorBase{
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+
+
+	public function index(){
+	
+		//Creamos el objeto usuario
+     	$vehiculos_embargados= new VehiculosEmbargadosModel(); 
+		
+	   //Conseguimos todos los usuarios
+		$resultSet=$vehiculos_embargados->getAll("id_vehiculos_embargados");
+				
+		$resultEdit = "";
+
+		
+		session_start();
+
+	
+		if (isset(  $_SESSION['usuario_usuarios']) )
+		{
+			$permisos_rol = new PermisosRolesModel();
+			$nombre_controladores = "VehiculosEmbargados";
+			$id_rol= $_SESSION['id_rol'];
+			$resultPer = $vehiculos_embargados->getPermisosVer("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+			
+			if (!empty($resultPer))
+			{
+				if (isset ($_GET["id_vehiculos_embargados"])   )
+				{
+
+					$nombre_controladores = "VehiculosEmbargados";
+					$id_rol= $_SESSION['id_rol'];
+					$resultPer = $vehiculos_embargados->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+						
+					if (!empty($resultPer))
+					{
+					
+						$_id_tipo_identificacion = $_GET["id_vehiculos_embargados"];
+						$columnas = " id_vehiculos_embargados, observaciones_vehiculos_embargados, placa_vehiculos_embargados ,modelo_vehiculos_embargados";
+						$tablas   = "vehiculos_embargados";
+						$where    = "id_vehiculos_embargados= '$_id_vehiculos_embargados' "; 
+						$id       = "observaciones_vehiculos_embargados";
+							
+						$resultEdit = $vehiculos_embargados->getCondiciones($columnas ,$tablas ,$where, $id);
+
+						$traza=new TrazasModel();
+						$_nombre_controlador = "VehiculosEmbargados";
+						$_accion_trazas  = "Editar";
+						$_parametros_trazas = $_id_vehiculos_embargados;
+						$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);
+					}
+					else
+					{
+						$this->view("Error",array(
+								"resultado"=>"No tiene Permisos de Editar Tipos de vehiculos embargados"
+					
+						));
+					
+					
+					}
+					
+				}
+		
+				
+				$this->view("VehiculosEmbargados",array(
+						"resultSet"=>$resultSet, "resultEdit" =>$resultEdit
+			
+				));
+		
+				
+				
+			}
+			else
+			{
+				$this->view("Error",array(
+						"resultado"=>"No tiene Permisos de Acceso a Tipos de Vehiculos Embargados"
+				
+				));
+				
+				exit();	
+			}
+				
+		}
+		else 
+		{
+				$this->view("ErrorSesion",array(
+						"resultSet"=>""
+			
+				));
+		
+		}
+	
+	}
+	
+	public function InsertaVehiculosEmbargados(){
+			
+		session_start();
+
+		
+		$vehiculos_embargados=new VehiculosEmbargadosModel();
+		$nombre_controladores = "VehiculosEmbargados";
+		$id_rol= $_SESSION['id_rol'];
+		$resultPer = $vehiculos_embargados->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+		
+		
+		if (!empty($resultPer))
+		{
+		
+		
+		
+			$resultado = null;
+			$vehiculos_embargados=new VehiculosEmbargadosModel();
+		
+			//_nombre_tipo_identificacion
+			
+			if (isset ($_POST["observaciones_vehiculos_embargados"]) )
+				
+			{
+				
+				
+				
+				$_observaciones_vehiculos_embargados= $_POST["observaciones_vehiculos_embargados"];
+				
+				if(isset($_POST["id_vehiculos_embargados"])) 
+				{
+					
+					$_id_vehiculos_embargados = $_POST["id_vehiculos_embargados"];
+					$colval = " observaciones_vehiculos_embargados= '$_observaciones_vehiculos_embargados'";
+					$tabla = "vehiculos_embargados";
+					$where = "id_vehiculos_embargados= '$_id_vehiculos_embargados'    ";
+					
+					$resultado=$vehiculos_embargados->UpdateBy($colval, $tabla, $where);
+					
+				}else {
+					
+			
+
+				
+				$funcion = "ins_vehiculos_embargados";
+				
+				$parametros = " '$_observaciones_vehiculos_embargados'  ";
+					
+				$vehiculos_embargados->setFuncion($funcion);
+		
+				$vehiculos_embargados->setParametros($parametros);
+		
+		
+				$resultado=$vehiculos_embargados->Insert();
+			 
+				$traza=new TrazasModel();
+				$_nombre_controlador = "Vehiculos Embargados";
+				$_accion_trazas  = "Guardar";
+				$_parametros_trazas = $_observaciones_vehiculos_embargados;
+				$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);
+				
+				}
+			 
+			 
+		
+			}
+			$this->redirect("vehiculos_embargados", "index");
+
+		}
+		else
+		{
+			$this->view("Error",array(
+					
+					"resultado"=>"No tiene Permisos de Insertar tipos de vehiculos embargados"
+		
+			));
+		
+		
+		}
+	
+
+		$vehiculos_embargados=new VehiculosEmbargadosModel();
+
+		$nombre_controladores = "VehiculosEmbargados";
+		$id_rol= $_SESSION['id_rol'];
+		$resultPer = $tipo_identificacion->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+		
+		
+		if (!empty($resultPer))
+		{
+		
+		
+		
+			$resultado = null;
+			$vehiculos_embargados=new VehiculosEmbargadosModel();
+		
+			//_nombre_tipo_identificacion
+			
+			if (isset ($_POST["observaciones_vehiculos_embargados"]) )
+				
+			{
+				$_observaciones_vehiculos_embargados= $_POST["observaciones_vehiculos_embargados"];
+				
+				if(isset($_POST["id_vehiculos_embargados"]))
+				{
+				$_id_vehiculos_embargados= $_POST["id_vehiculos_embargados"];
+				$colval = " observaciones_vehiculos_embargados= '$_observaciones_vehiculos_embargados'   ";
+				$tabla = "vehiculos_embargados";
+				$where = "id_vehiculos_embargados= '$_id_vehiculos_embargados'    ";
+					
+				$resultado=$vehiculos_embargados->UpdateBy($colval, $tabla, $where);
+					
+				}else {
+				
+			
+				$funcion = "ins_vehiculos_embargados";
+				
+				$parametros = " '$_observaciones_vehiculos_embargados'  ";
+					
+				$vehiculos_embargados->setFuncion($funcion);
+		
+				$vehiculos_embargados->setParametros($parametros);
+		
+		
+				$resultado=$vehiculos_embargados->Insert();
+			 }
+		
+			}
+			$this->redirect("VehiculosEmbargados", "index");
+
+		}
+		else
+		{
+			$this->view("Error",array(
+					
+					"resultado"=>"No tiene Permisos de Insertar tipos de vehiculos embargados"
+		
+			));
+		
+		
+		}
+		
+	}
+
+
+
+
+	public function borrarId()
+	{
+
+		session_start();
+		
+		$permisos_rol=new PermisosRolesModel();
+		$nombre_controladores = "Roles";
+		$id_rol= $_SESSION['id_rol'];
+		$resultPer = $permisos_rol->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+			
+		if (!empty($resultPer))
+		{
+			if(isset($_GET["id_vehiculos_embargados"]))
+			{
+				$id_vehiculos_embargados=(int)$_GET["id_vehiculos_embargados"];
+				
+				$tipo_vehiculos_embargados=new VehiculosEmbargadosModel();
+				
+				$tipo_vehiculos_embargados->deleteBy(" id_vehiculos_embargados",$id_vehiculos_embargados);
+				
+				$traza=new TrazasModel();
+				$_nombre_controlador = "Vehiculos Embargados";
+				$_accion_trazas  = "Borrar";
+				$_parametros_trazas = $id_vehiculos_embargados;
+				$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);
+			}
+			
+			$this->redirect("VehiculosEmbargados", "index");
+			
+			
+		}
+		else
+		{
+			$this->view("Error",array(
+				"resultado"=>"No tiene Permisos de Borrar Tipos de vehiculos embargados"
+			
+			));
+		}
+				
+	}
+	
+	
+	public function Reporte(){
+	
+		//Creamos el objeto usuario
+		$vehiculos_embargados=new VehiculosEmbargadosModel();
+		//Conseguimos todos los usuarios
+		
+	
+	
+		session_start();
+	
+	
+		if (isset(  $_SESSION['usuario']) )
+		{
+			$resultRep = $roles->getByPDF("id_rol, nombre_rol", " nombre_rol != '' ");
+			$this->report("VehiculosEmbargados",array(	"resultRep"=>$resultRep));
+	
+		}
+					
+	
+	}
+	
+	
+	
+}
+?>
---
+++ FrameworkMVC/view/TipoGastosView.php
@@ -0,0 +1,209 @@
+
+   <!DOCTYPE HTML>
+<html lang="es">
+
+      <head>
+      
+        <meta charset="utf-8"/>
+        <title>Tipo de gastos - aDocument 2015</title>
+        
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		  			   
+          <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	      <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		
+		<link rel="stylesheet" href="http://jqueryvalidation.org/files/demo/site-demos.css">
+        <script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>
+        <script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+ 		
+ 		<script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
+		
+		<script>
+		    webshims.setOptions('forms-ext', {types: 'date'});
+			webshims.polyfill('forms forms-ext');
+		</script>
+		
+        
+        
+       <style>
+            input{
+                margin-top:5px;
+                margin-bottom:5px;
+            }
+            .right{
+                float:right;
+            }
+                
+            
+        </style>
+         
+         <script >
+		$(document).ready(function(){
+
+		    // cada vez que se cambia el valor del combo
+		    $("#Guardar").click(function() 
+			{
+		   
+		    	var nombre_tipo_gastos = $("#nombre_tipo_gastos").val();
+		    
+		   				
+		    	if (nombre_tipo_gastos == "")
+		    	{
+			    	
+		    		$("#mensaje_nombres").text("Introduzca un tipo de gastos ");
+		    		$("#mensaje_nombres").fadeIn("slow"); //Muestra mensaje de error
+		            return false;
+			    }
+		    	else 
+		    	{
+		    		$("#mensaje_nombres").fadeOut("slow"); //Muestra mensaje de error
+		            
+				}
+		    	
+		    	
+
+			
+		    					    
+
+			}); 
+
+
+		 
+				
+				$( "#nombre_tipo_gastos" ).focus(function() {
+					$("#mensaje_nombres").fadeOut("slow");
+    			});
+				
+			
+		
+				
+		
+		      
+				    
+		}); 
+
+	</script>
+
+    </head>
+    <body style="background-color: #d9e3e4;">
+    
+       <?php include("view/modulos/head.php"); ?>
+       
+       <?php include("view/modulos/menu.php"); ?>
+       
+       
+       
+       <?php
+       
+       
+       
+		   
+		?>
+ 
+  
+  <div class="container">
+  
+  <div class="row" style="background-color: #ffffff;">
+  
+       <!-- empieza el form --> 
+       
+      <form action="<?php echo $helper->url("TipoGastos","InsertaTipoGastos"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-6">
+            
+         
+        	    <h4 style="color:#ec971f;">Insertar Tipos de Gastos</h4>
+            	<hr/>
+            	
+		   		
+            
+          <?php if ($resultEdit !="" ) { foreach($resultEdit as $resEdit) {?>
+            
+            
+            
+        
+			   
+			   <div class="row">
+		       <div class="col-xs-12 col-md-12">
+			  	<p  class="formulario-subtitulo" >Nombres tipos de Gastos</p>
+			  	<input type="text"  name="nombre_tipo_gastos" id="nombre_tipo_gastos" value="<?php echo $resEdit->nombre_tipo_gastos; ?>" class="form-control"/> 
+			  	<input type="hidden"  name="id_tipo_gastos"  value="<?php echo $resEdit->id_tipo_gastos; ?>" class="form-control"/> 
+			    <div id="mensaje_nombres" class="errores"></div>
+			  </div>
+			   </div>
+		    
+		     <?php } } else {?>
+		    
+			   <div class="row">
+		       <div class="col-xs-6 col-md-6">
+			  	<p  class="formulario-subtitulo" >Nombres tipos de Gastos</p>
+			  	<input type="text"  name="nombre_tipo_gastos" id="nombre_tipo_gastos" value="" class="form-control"/> 
+			    <div id="mensaje_nombres" class="errores"></div>
+			  </div>
+			 </div>
+
+		    <hr>
+		    
+		   
+               	
+		     <?php } ?>
+		     
+		     
+		       <div class="row">
+			  <div class="col-xs-12 col-md-6" style="text-align: center;" >
+			  	<input type="submit" id="Guardar" name="Guardar" value="Guardar" class="btn btn-success"/>
+			  </div>
+			</div>     
+               
+		
+		 <hr>
+          
+       </form>
+       <!-- termina el form --> 
+       
+        <div class="col-lg-6">
+            <h4 style="color:#ec971f;">Lista de Tipos de Gastos</h4>
+            <hr/>
+        </div>
+        <section class="col-lg-6 usuario" style="height:400px;overflow-y:scroll;">
+        <table class="table table-hover ">
+	         <tr >
+	    		<th style="color:#456789;font-size:80%;"><b>Id</b></th>
+	    		<th style="color:#456789;font-size:80%;">Nombre</th>
+	    		
+	    		<th></th>
+	    		<th></th>
+	  		</tr>
+            
+	            <?php if (!empty($resultSet)) {  foreach($resultSet as $res) {?>
+	        		<tr>
+	                   <td style="color:#000000;font-size:80%;"> <?php echo $res->id_tipo_gastos; ?></td>
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_tipo_gastos; ?>     </td> 
+		              
+		           	   <td>
+			           		<div class="right">
+			                    <a href="<?php echo $helper->url("TipoGastos","index"); ?>&id_tipo_gastos=<?php echo $res->id_tipo_gastos; ?>" class="btn btn-warning" style="font-size:65%;">Editar</a>
+			                </div>
+			            
+			             </td>
+			             <td>   
+			                	<div class="right">
+			                    <a href="<?php echo $helper->url("TipoGastos","borrarId"); ?>&id_tipo_gastos=<?php echo $res->id_tipo_gastos; ?>" class="btn btn-danger" style="font-size:65%;">Borrar</a>
+			                </div>
+			                <hr/>
+		               </td>
+		    		</tr>
+		        <?php } } ?>
+            
+            <?php 
+            
+            //echo "<script type='text/javascript'> alert('Hola')  ;</script>";
+            
+            ?>
+            
+       	</table>     
+      </section>
+      </div>
+      </div>
+   </body>  
+
+    </html>   
