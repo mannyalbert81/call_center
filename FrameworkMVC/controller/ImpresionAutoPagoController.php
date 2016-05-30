9bcd57e72fcaeff3afad8e68cb0b61471dc69acc
mAYCOL <maycol@masoft.net>
2016-05-30 16:04:58 -0500

    cambios

--- FrameworkMVC/controller/ImpresionAutoPagoController.php
+++ FrameworkMVC/controller/ImpresionAutoPagoController.php
@@ -113,7 +113,8 @@ class ImpresionAutoPagoController extends ControladorBase{
 						$impresion_auto_pago = new AutoPagosModel();
 							
 							
-						$columnas = " titulo_credito.id_titulo_credito, 
+						$columnas = " auto_pagos.id_auto_pagos,
+									  titulo_credito.id_titulo_credito, 
 									  juicios.juicio_referido_titulo_credito, 
 									  clientes.identificacion_clientes, 
 									  clientes.nombres_clientes, 
@@ -219,24 +220,94 @@ class ImpresionAutoPagoController extends ControladorBase{
 						  titulo_credito.total, 
 						  juicios.juicio_referido_titulo_credito, 
 						  clientes.nombres_clientes,
-				          ciudad.nombre_ciudad";
+				          ciudad.nombre_ciudad,
+				          juicios.creado";
 			
 		$tablas   = "  public.clientes, 
 					  public.titulo_credito, 
 					  public.juicios,
-				      public.ciudad";
+				      public.ciudad,
+					  public.auto_pagos";
 			
 		$where    = "titulo_credito.id_clientes = clientes.id_clientes AND
   						titulo_credito.id_titulo_credito = juicios.id_titulo_credito AND
-				         ciudad.id_ciudad = clientes.id_ciudad";
+				         ciudad.id_ciudad = clientes.id_ciudad AND auto_pagos.id_titulo_credito=titulo_credito.id_titulo_credito";
 			
 		$id       = "clientes.id_tipo_identificacion";
 		
 		$resultCliente=$cliente->getCondiciones($columnas ,$tablas ,$where, $id);
 		
 		
+		$usuario= new UsuariosModel();
+	
+		$colusuario="auto_pagos.id_usuario_impulsor,
+					usuarios.nombre_usuarios";
+		$tblusuario="public.auto_pagos,
+					public.juicios,
+					public.titulo_credito,
+					public.usuarios";
+		$whereusuario="auto_pagos.id_titulo_credito = juicios.id_titulo_credito AND
+					auto_pagos.id_usuario_impulsor = usuarios.id_usuarios AND
+					juicios.id_titulo_credito = titulo_credito.id_titulo_credito";
+		$idusuario="auto_pagos.id_usuario_impulsor";
 		
-		$this->report("ImpresionAutoPago",array( "resultCliente"=>$resultCliente
+		$resultUsuario=$usuario->getCondiciones($colusuario, $tblusuario, $whereusuario, $idusuario);
+		
+		$id_impulsor=$resultUsuario[0]->id_usuario_impulsor;
+		
+		
+		$columnas_secretario="B.id_asignacion_secretarios AS id_asignacion_secretarios ,B.id_secretario_asignacion_secretarios AS id_secretario,B.id_abogado_asignacion_secretarios AS id_abogado,
+		(SELECT A.nombre_usuarios FROM usuarios A WHERE A.id_usuarios=B.id_secretario_asignacion_secretarios) AS secretarios,
+		(SELECT A.nombre_usuarios FROM usuarios A WHERE A.id_usuarios=B.id_abogado_asignacion_secretarios) AS impulsadores";
+		$tbl_secretario="asignacion_secretarios B";
+		$where_secretario="B.id_abogado_asignacion_secretarios='$id_impulsor'";
+		$id_secretario="B.id_asignacion_secretarios";
+			
+		$resultSecretario=$usuario->getCondiciones($columnas_secretario, $tbl_secretario, $where_secretario, $id_secretario);
+		
+		$id_abogado=$resultSecretario[0]->id_abogado;
+		$id_secretario=$resultSecretario[0]->id_secretario;
+	
+		$firmas = new FirmasDigitalesModel();
+		
+		$columna_firmas_abogado="firmas_digitales.id_firmas_digitales,
+		firmas_digitales.imagen_firmas_digitales";
+		$tbla_firmas_abogado="public.firmas_digitales";
+		$where_firmas_abogado="firmas_digitales.id_usuarios='$id_abogado'";
+		$id_firmas_abogado="id_firmas_digitales";
+		
+		$resultFirma_abogado=$firmas->getCondiciones($columna_firmas_abogado, $tbla_firmas_abogado, $where_firmas_abogado, $id_firmas_abogado);
+		
+		
+		$columna_firmas_secretario="firmas_digitales.id_firmas_digitales,
+		firmas_digitales.imagen_firmas_digitales";
+		$tbla_firmas_secretario="public.firmas_digitales";
+		$where_firmas_secretario="firmas_digitales.id_usuarios='$id_secretario'";
+		$id_firmas_secretario="id_firmas_digitales";
+		
+		$resultFirma_secretario=$firmas->getCondiciones($columna_firmas_secretario, $tbla_firmas_secretario, $where_firmas_secretario, $id_firmas_secretario);
+		
+		$liquidador= new UsuariosModel();
+		
+		$columnas = " usuarios.id_usuarios, 
+					  firmas_digitales.id_firmas_digitales, 
+					  usuarios.nombre_usuarios, 
+					  firmas_digitales.imagen_firmas_digitales, 
+					  rol.nombre_rol";
+			
+		$tablas   = " public.usuarios, 
+					  public.firmas_digitales, 
+					  public.rol ";
+			
+		$where    = "firmas_digitales.id_usuarios = usuarios.id_usuarios AND
+  					rol.id_rol = usuarios.id_rol AND nombre_rol='LIQUIDADOR'";
+			
+		$id       = "usuarios.nombre_usuarios";
+		
+		$resultLiquidador=$liquidador->getCondiciones($columnas ,$tablas ,$where, $id);
+		
+		
+		$this->report("ImpresionAutoPago",array( "resultCliente"=>$resultCliente,"resultSecretario"=>$resultSecretario,"resultFirma_abogado"=>$resultFirma_abogado,"resultFirma_secretario"=>$resultFirma_secretario, "resultLiquidador"=> $resultLiquidador
 		
 		
 		));
--- FrameworkMVC/view/reportes/ImpresionAutoPagoReport.php
+++ FrameworkMVC/view/reportes/ImpresionAutoPagoReport.php
@@ -1,6 +1,6 @@
 <?php
 
-$base_url = "http://186.71.249.121:4000/Vademano/";
+$base_url = "http://localhost:4000/FrameworkMVC/";
 
 
 
@@ -21,8 +21,52 @@ foreach($resultCliente as $res)
 	$juicio_referido_titulo_credito			           =$res->juicio_referido_titulo_credito;
 	$nombres_clientes			                       =$res->nombres_clientes;
 	$nombre_ciudad                                      =$res->nombre_ciudad;
+	$creado                                             = date('Y-m-d ',strtotime($res->creado));
+	
+	
+	
 }
 
+
+
+foreach($resultSecretario as $res)
+{
+	$id_impulsor				=$res->id_abogado;
+	$nombre_impulsor			=$res->impulsadores;
+	$id_secretario				=$res->id_secretario;
+	$nombre_secretario			=$res->secretarios;
+	
+}
+$logo_impulsore ="";
+foreach($resultFirma_abogado as $res)
+{
+	$id_firma_abogado			=$res->id_firmas_digitales;
+	$firma_abogado				=$res->imagen_firmas_digitales;
+	
+	$logo_impulsores 			= '<img src="'. $base_url.'view/DevuelveImagen.php?id_valor='.$id_firma_abogado.'&id_nombre=id_firmas_digitales&tabla=firmas_digitales&campo=imagen_firmas_digitales" width="120" height="80" />';
+	
+}
+
+$logo_secretarios="";
+foreach($resultFirma_secretario as $res)
+{
+	$id_firma_secretario			=$res->id_firmas_digitales;
+	$firma_secretario				=$res->imagen_firmas_digitales;
+	
+	$logo_secretarios 				= '<img src="'. $base_url.'view/DevuelveImagen.php?id_valor='.$id_firma_secretario.'&id_nombre=id_firmas_digitales&tabla=firmas_digitales&campo=imagen_firmas_digitales" width="120" height="80" />';
+	
+}
+
+$logo_liquidador ="";
+foreach($resultLiquidador as $res)
+{
+	$id_usuarios                   =$res->id_usuarios;
+	$nombre_liquidador             =$res->nombre_usuarios;
+	$firma_liquidador             =$res->imagen_firmas_digitales;
+	
+	$logo_liquidador 				= '<img src="'. $base_url.'view/DevuelveImagen.php?id_valor='.$id_usuarios.'&id_nombre=id_usuarios&tabla=firmas_digitales&campo=imagen_firmas_digitales" width="120" height="80" />';
+	
+}
     
 
 
@@ -53,7 +97,7 @@ $html =
 '</div>'.
 
 '<div style="margin-top:0px; color:#000000; font-family: sans-serif; font-size:75%; width:100%; text-align: center;">'.
-'<strong>'.'<font size=2>'. $nombre_ciudad . '<font>'. '</strong>'.
+'<strong>'.'<font size=2>'. $nombre_ciudad . '<<font color="Gray"></font>>'. '</strong>'.
 '</div>'.
 
 '<div style="margin-top:20px; color:#000000; font-family: sans-serif; font-size:75%; width:100%; text-align: center;">'.
@@ -69,7 +113,7 @@ $html =
 '</div>'.
 
 '<div style="margin-left:20px; margin-top:20px; color:#000000; font-family: sans-serif; font-size:75%; width:100%; text-align: left;">'.
-'<strong>'.'<font size=2>'.  'FECHA DE INICIO: 2016-05-26'. '<font>'. '</strong>'.
+'<strong>'.'<font size=2>'.  'FECHA DE INICIO: ' .$creado. '<font>'. '</strong>'.
 '</div>'.
 
 '<div style="margin-left:20px; margin-top:20px; color:#000000; font-family: sans-serif; font-size:75%; width:100%; text-align: left;">'.
@@ -123,14 +167,39 @@ $html =
 '</div>'.
 
 
+'<div align="center" style= "margin-top:60px">'.
+'<center>'.
+'<table border="0"  width="100"  align="center">'.
 
 
+'<tr>'.
+'<td align="center">'.$logo_impulsores . '</td>'.
+'<td align="center" style="color:#ffffff">'.'......'.'</td>'.
+'<td align="center">'.$logo_liquidador . '</td>'.
+'<td align="center" style="color:#ffffff">'.'......'.'</td>'.
+'<td align="center">'.$logo_secretarios .'</td>'.
+'</tr>'.
+'<tr>'.
+'<td align="center">'. '<strong>'.'<font size=2>'.  'IMPULSOR '. '<font>'. '</strong>'.'</td>'.
+'<td align="center" style="color:#ffffff">'.'......'.'</td>'.
+'<td align="center">'. '<strong>'.'<font size=2>'.  'LIQUIDADOR '. '<font>'. '</strong>'.'</td>'.
+'<td align="center" style="color:#ffffff">'.'......'.'</td>'.
+'<td align="center">'. '<strong>'.'<font size=2>'.  'SECRETARIO '. '<font>'. '</strong>'.'</td>'.
+'</tr>'.
+'<tr>'.
+'<td align="center">'. '<strong>'.'<font size=2>'.'Ab. '. $nombre_impulsor. '<font>'. '</strong>'.'</td>'.
+'<td align="center" style="color:#ffffff">'.'......'.'</td>'.
+'<td align="center">'. '<strong>'.'<font size=2>'.'Dr. '. $nombre_liquidador . '<font>'. '</strong>'.'</td>'.
+'<td align="center" style="color:#ffffff">'.'......'.'</td>'.
+'<td align="center">'. '<strong>'.'<font size=2>'.'Ab. '. $nombre_secretario . '<font>'. '</strong>'.'</td>'.
+'</tr>'.
 
+'</table>'.
+'</center>'.
+'</div>'.
 
-
-
-
-
+'<div align="center" style= "margin-top:120px">'.
+'</div>'.
 
 
 '</div>'.
