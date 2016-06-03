31adc9961075694eef83f8b6147e6d0eb45d2005
ManuelAlberto <manuel@masoft.net>
2016-06-03 16:30:02 -0500

    reportes

---
+++ FrameworkMVC/view/ireports/conexion.php
@@ -0,0 +1,13 @@
+<?php 
+
+define("server", "192.168.100.3");
+define("db", "coactiva");
+define("user", "postgres");
+define("pass", ".Romina.2012");
+define("version", "0.8b");
+define("pchartfolder", "./class/pchart2");
+define("driver", "psql");
+
+
+
+?>
