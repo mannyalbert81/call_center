a8a76490bef3dbb21d523b98975112e1c974af58
steven <steven@masoft.net>
2016-06-07 12:25:34 -0500

    ghgf

--- FrameworkMVC/view/ireports/ContUsuariosReport.php
+++ FrameworkMVC/view/ireports/ContUsuariosReport.php
@@ -17,11 +17,8 @@ ini_set('display_errors', 0);
   
 #aqu� va el reporte
 
-$xml =  simplexml_load_file("VehiculosReport.jrxml");
+$xml =  simplexml_load_file("UsuariosReport.jrxml");
 
-$xml =  simplexml_load_file("Usuarios2.jrxml");
-
-  
 $PHPJasperXML = new PHPJasperXML();
 $PHPJasperXML->xml_dismantle($xml); 
 
