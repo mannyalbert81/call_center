0636d205563b75514a1d15225018ba90dae081ff
mAYCOL <maycol@masoft.net>
2016-06-09 17:52:24 -0500

    reertret

---
+++ FrameworkMVC/view/ireports/ContClientesReport.php
@@ -0,0 +1,26 @@
+<?php 
+#<?php 
+#Importas la librer�a PhpJasperLibrary
+include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
+include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
+include_once ('conexion.php');
+
+
+#Conectas a la base de datos 
+$server  = server;
+$user    = user;
+$pass    = pass;
+$db      = db;
+$driver  = driver;
+ini_set('display_errors', 0);
+  
+#aqu� va el reporte
+
+$xml =  simplexml_load_file("ClientesReport.jrxml");
+
+
+$PHPJasperXML = new PHPJasperXML();
+$PHPJasperXML->xml_dismantle($xml); 
+$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
+$PHPJasperXML->outpage("I");
+?>
