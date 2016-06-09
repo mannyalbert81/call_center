a2c5feb0faebdf2909e84b23ae743be93b649f88
steven <steven@masoft.net>
2016-06-09 17:56:50 -0500

    DSFDSF

--- FrameworkMVC/view/ireports/ContOficiosReport.php
+++ FrameworkMVC/view/ireports/ContOficiosReport.php
@@ -14,7 +14,7 @@ ini_set('display_errors', 0);
   
 #aqu� va el reporte
 
-$xml =  simplexml_load_file("Oficios2Report.jrxml");
+$xml =  simplexml_load_file("OficiosReport.jrxml");
 
 
 $PHPJasperXML = new PHPJasperXML();
