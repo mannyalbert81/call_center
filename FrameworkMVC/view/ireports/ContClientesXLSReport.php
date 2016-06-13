5ff58dd6e50e11c5b5a062b08150ad128ec79e12
mAYCOL <maycol@masoft.net>
2016-06-13 17:40:34 -0500

    GDGDF

---
+++ FrameworkMVC/view/ireports/ContClientesXLSReport.php
@@ -0,0 +1,39 @@
+<?php 
+#<?php 
+#Importas la librer�a PhpJasperLibrary
+ob_end_clean(); //add this line here
+
+include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
+include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
+include_once("PhpJasperLibrary/class/ExportXLS.inc.php");
+include_once("PhpJasperLibrary/class/PHPExcel.php");
+
+include_once ('conexion.php');
+
+
+#Conectas a la base de datos 
+$server  = server;
+$user    = user;
+$pass    = pass;
+$db      = db;
+//$driver  = driver;
+ini_set('display_errors', 0);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+$PHPJasperXML->debugsql=FALSE;
+#aqu� va el reporte
+
+
+$id=$_GET['id_clientes'];
+$PHPJasperXML = new PHPJasperXML("en","XLS");
+$PHPJasperXML->debugsql=FALSE;
+$PHPJasperXML->arrayParameter=array("_id_clientes"=>$id);
+$PHPJasperXML->load_xml_file("ClientesXLSReport.jrxml");
+
+
+////$PHPJasperXML = new PHPJasperXML();
+////$PHPJasperXML->xml_dismantle($xml); 
+$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db);
+$PHPJasperXML->outpage("I");
+
+
+?>
