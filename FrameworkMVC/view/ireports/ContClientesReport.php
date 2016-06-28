ed8acf6ea36e8ffff9623c1b0d2a0fd0d59f73e2
steven <steven@masoft.net>
2016-06-28 12:22:12 -0500

    fgdf

--- FrameworkMVC/view/ireports/ContClientesReport.php
+++ FrameworkMVC/view/ireports/ContClientesReport.php
@@ -1,10 +1,11 @@
-<?php 
-#<?php 
+<?php
+
+#<?php
 #Importas la librer�a PhpJasperLibrary
 include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
 include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
-include_once ('conexion.php');  
-#Conectas a la base de datos 
+include_once ('conexion.php');
+#Conectas a la base de datos
 $server  = server;
 $user    = user;
 $pass    = pass;
@@ -12,14 +13,14 @@ $db      = db;
 $driver  = driver;
 ini_set('display_errors', 0);
 
+#aqu� va el reporte
 
-$xml =  simplexml_load_file("ClientesReport.jrxml");
-
-
+$directorio = $_SERVER['DOCUMENT_ROOT'].'/documentos/Reportes/';
 $PHPJasperXML = new PHPJasperXML();
-$PHPJasperXML->xml_dismantle($xml); 
+//$PHPJasperXML->debugsql=true;
+$PHPJasperXML->arrayParameter=array();
+$PHPJasperXML->load_xml_file("ClientesReport.jrxml");
+
 $PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
-$PHPJasperXML->outpage("I");
-
-
+$PHPJasperXML->outpage("F",$directorio.'Clientes.pdf'); //page output method I:standard output D:Download file, F =save as filename and submit 2nd parameter as destinate file name /$PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
 ?>
--- FrameworkMVC/view/ireports/ContOficiosReport.php
+++ FrameworkMVC/view/ireports/ContOficiosReport.php
@@ -1,21 +1,26 @@
-<?php 
-#<?php 
+<?php
+
+#<?php
 #Importas la librer�a PhpJasperLibrary
 include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
 include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
-include_once ('conexion.php');  
-#Conectas a la base de datos 
+include_once ('conexion.php');
+#Conectas a la base de datos
 $server  = server;
 $user    = user;
 $pass    = pass;
 $db      = db;
 $driver  = driver;
 ini_set('display_errors', 0);
-  
+
 #aqu� va el reporte
-$xml =  simplexml_load_file("OficiosReport.jrxml");
+
+$directorio = $_SERVER['DOCUMENT_ROOT'].'/documentos/Reportes/';
 $PHPJasperXML = new PHPJasperXML();
-$PHPJasperXML->xml_dismantle($xml); 
+//$PHPJasperXML->debugsql=true;
+$PHPJasperXML->arrayParameter=array();
+$PHPJasperXML->load_xml_file("OficiosReport.jrxml");
+
 $PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
-$PHPJasperXML->outpage("I");
+$PHPJasperXML->outpage("F",$directorio.'Oficios.pdf'); //page output method I:standard output D:Download file, F =save as filename and submit 2nd parameter as destinate file name /$PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
 ?>
