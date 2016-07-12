527dbffab84a8158671abf36e286e408a5ca44ca
mAYCOL <maycol@masoft.net>
2016-07-12 15:20:58 -0500

    DDFDSF

--- FrameworkMVC/view/DocumentosView.php
+++ FrameworkMVC/view/DocumentosView.php
@@ -141,10 +141,10 @@
             	<hr/>
        <!-- empieza el form --> 
        
-      <form action="<?php echo $helper->url("Documentos","index"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-6">
+      <form action="<?php echo $helper->url("Documentos","index"); ?>" method="post" enctype="multipart/form-data">
             
          
-            	
+            <div class="col-lg-6">	
 		   		
             
           <?php if ($resultEdit !="" ) { foreach($resultEdit as $resEdit) {?>
@@ -193,7 +193,7 @@
 		      <div class="col-xs-4 col-md-4" style="margin-top:10px">
 			  <p  class="formulario-subtitulo" >Fecha de Providencia:</p>
 					   
-	          <input type="date" id="fecha_emision_documentos" name="fecha_emision_documentos" value="<?php echo date("Y-n-j"); ?>" class="form-control" required>
+	          <input type="date" id="fecha_emision_documentos" name="fecha_emision_documentos" value="<?php  $fecha=date("d/m/y");  echo $fecha;?>" class="form-control">
 		   	   	<div id="mensaje_criterio" class="errores"></div>	   
 		    </div>
 		    
@@ -207,60 +207,28 @@
 		    
 		    <?php } ?>
 		     
-		      <div class="row">
+		     </div>
+		     
+         <div class="col-lg-6">
+             <div class="row">
+              <div class="col-xs-4 col-md-4" style="margin-top:10px">
+		      <textarea id="textarea_comunicacion" name="comunicacion" rows="8" cols="70">VISTOS: </textarea>
+		      </div>
+		      </div>
+        </div>
+          
+          
+          
+          <div class="row">
 			  <div class="col-xs-12 col-md-6" style="text-align: center;" >
 			  <input type="submit" id="Guardar" name="Guardar" value="Guardar" onClick="Ok()" class="btn btn-success"/>
 			  </div>
-			  </div>     
-              
-            
-		 <hr>
-          
+			  </div>    
        </form>
        <!-- termina el form --> 
        
-        <div class="col-lg-6">
-            <h4 style="color:#ec971f;">Lista de Ciudades</h4>
-            <hr/>
-        </div>
-        <section class="col-lg-6 usuario" style="height:400px;overflow-y:scroll;">
-        <table class="table table-hover ">
-	         <tr >
-	    		<th style="color:#456789;font-size:80%;"><b>Id</b></th>
-	    		<th style="color:#456789;font-size:80%;">Nombre</th>
-	    		
-	    		<th></th>
-	    		<th></th>
-	  		</tr>
-            
-	            <?php if (!empty($resultSet)) {  foreach($resultSet as $res) {?>
-	        		<tr>
-	                   <td style="color:#000000;font-size:80%;"> <?php echo $res->id_ciudad; ?></td>
-		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_ciudad; ?>     </td> 
-		              
-		           	   <td>
-			           		<div class="right">
-			                    <a href="<?php echo $helper->url("Ciudad","index"); ?>&id_ciudad=<?php echo $res->id_ciudad; ?>" class="btn btn-warning" onClick="notificacion()" style="font-size:65%;">Editar</a>
-			                </div>
-			            
-			             </td>
-			             <td>   
-			                	<div class="right">
-			                    <a href="<?php echo $helper->url("Ciudad","borrarId"); ?>&id_ciudad=<?php echo $res->id_ciudad; ?>" class="btn btn-danger" onClick="Borrar()" style="font-size:65%;">Borrar</a>
-			                </div>
-			                <hr/>
-		               </td>
-		    		</tr>
-		        <?php } } ?>
-            
-            <?php 
-            
-            //echo "<script type='text/javascript'> alert('Hola')  ;</script>";
-            
-            ?>
-            
-       	</table>     
-      </section>
+        
+        
       </div>
       </div>
    </body>  
