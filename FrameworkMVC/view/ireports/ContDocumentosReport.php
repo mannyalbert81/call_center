18877d0f6926d056b06482d7b13f34dff5e9b079
steven <steven@masoft.net>
2016-07-13 10:16:12 -0500

    ghgh

--- FrameworkMVC/view/ireports/ContDocumentosReport.php
+++ FrameworkMVC/view/ireports/ContDocumentosReport.php
@@ -13,6 +13,18 @@ $db      = db;
 $driver  = driver;
 ini_set('display_errors', 0);
 
+$variable_nombre_archivo = '';
+
+
+////ver que tipo de documentos es
+
+
+//// recuperar el nombre del cliente + el juicio + fecha y hora actual
+
+///repositorio
+
+/////buscar en la tabla de la base en docu,mentos y guardar el camino + nombre del documentos
+
 #aqu� va el reporte
 $directorio = $_SERVER['DOCUMENT_ROOT'].'/documentos/Reportes/';
 
@@ -26,9 +38,12 @@ $PHPJasperXML->outpage("I",$directorio.'Providencia.pdf');
 $directorio = $_SERVER['DOCUMENT_ROOT'].'/documentos/Reportes/';
 $PHPJasperXML = new PHPJasperXML();
 
+
+
+
 //$PHPJasperXML->debugsql=true;
 $PHPJasperXML->arrayParameter=array();
 $PHPJasperXML->load_xml_file("DocumentosReport.jrxml");
 $PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
-$PHPJasperXML->outpage("F",$directorio.'Providencia.pdf'); //page output method I:standard output D:Download file, F =save as filename and submit 2nd parameter as destinate file name /$PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
+$PHPJasperXML->outpage("F",$directorio. $variable_nombre_archivo .'.pdf'); //page output method I:standard output D:Download file, F =save as filename and submit 2nd parameter as destinate file name /$PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
 ?>
