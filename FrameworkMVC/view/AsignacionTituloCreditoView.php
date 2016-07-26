63425df8889acf336265f8b6b19761cebbfcca50
mAYCOL <maycol@masoft.net>
2016-07-26 10:56:24 -0500

    DSDASDSA

--- FrameworkMVC/view/AsignacionTituloCreditoView.php
+++ FrameworkMVC/view/AsignacionTituloCreditoView.php
@@ -65,6 +65,42 @@
         </style>
        
        
+        <script >
+		    // cada vez que se cambia el valor del combo
+		    $(document).ready(function(){
+		    
+		    $("#Guardar").click(function() 
+			{
+		    
+		    	var id_ciudad= $("#id_ciudad").val();
+		     		    	
+		    	
+		    	if (id_ciudad == 0)
+		    	{
+			    	
+		    		$("#mensaje_id_ciudad").text("Seleccione una Ciudad");
+		    		$("#mensaje_id_ciudad").fadeIn("slow"); //Muestra mensaje de error
+		            return false;
+			    }
+		    	else 
+		    	{
+		    		$("#mensaje_id_ciudad").fadeOut("slow"); //Muestra mensaje de error
+		            
+				}    
+				
+		
+			}); 
+
+
+		        $( "#id_ciudad" ).focus(function() {
+				  $("#mensaje_id_ciudad").fadeOut("slow");
+			    });
+				
+				
+		}); 
+
+	</script>
+       
        
      <script>
 	$(document).ready(function(){
@@ -119,12 +155,8 @@
 	</script>
     
          
-         
-
-      
-			
-			 <script >
-$(document).ready(function() {
+	 <script >
+          $(document).ready(function() {
 		
 		$('#Guardar').click(function(){
 	        var selected = '';  
@@ -149,182 +181,6 @@ $(document).ready(function() {
 	});
 	</script>
 
-        <script >
-		$(document).ready(function(){
-
-			$("#Buscar").click(function()
-
-			{
-
-				var contenido = $("#contenido").val();
-				var criterio= $("#criterio").val();
-
-				if (contenido != "" && criterio==0)
-		    	{
-					$("#mensaje_criterio").text("Seleccione filtro de busqueda");
-		    		$("#mensaje_criterio").fadeIn("slow"); //Muestra mensaje de error
-		            return false;
-			    }
-		    	else 
-		    	{
-		    		$("#mensaje_criterio").fadeOut("slow"); //Muestra mensaje de error
-		    		
-		            
-				}    
-
-				if (criterio !=0 && contenido=="")
-		    	{
-
-			    	
-		    		$("#mensaje_contenido").text("Ingrese Contenido a buscar");
-		    		$("#mensaje_contenido").fadeIn("slow"); //Muestra mensaje de error
-		            return false;
-			    	
-		    		
-			    }
-		    	else 
-		    	{
-		    		$("#mensaje_contenido").fadeOut("slow"); //Muestra mensaje de error
-		            
-				}    
-
-
-				
-
-		
-				
-			});
-
-
-			$( "#contenido" ).focus(function() {
-				  $("#mensaje_contenido").fadeOut("slow");
-			    });
-
-			$( "#criterio" ).focus(function() {
-				  $("#mensaje_criterio").fadeOut("slow");
-			    });
-		   
-			
-		});
-			</script >
-
-        
-    
-    		
-			<script >
-		    // cada vez que se cambia el valor del combo
-		    $(document).ready(function(){
-		    
-		    $("#Guardar").click(function() 
-			{
-		    	var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
-		    	var validaFecha = /([0-9]{4})\-([0-9]{2})\-([0-9]{2})/;
-
-		    	var identificacion_clientes = $("#identificacion_clientes").val();
-		    	var nombres_clientes = $("#nombres_clientes").val();
-		    	var telefono_clientes = $("#telefono_clientes").val();
-		    	var celular_clientes = $("#celular_clientes").val();
-		    	var direccion_clientes = $("#direccion_clientes").val();
-		    	
-		    	
-		    	
-		    	if (identificacion_clientes == "")
-		    	{
-			    	
-		    		$("#mensaje_identificacion_clientes").text("Introduzca una Identificacion");
-		    		$("#mensaje_identificacion_clientes").fadeIn("slow"); //Muestra mensaje de error
-		            return false;
-			    }
-		    	else 
-		    	{
-		    		$("#mensaje_identificacion_clientes").fadeOut("slow"); //Muestra mensaje de error
-		            
-				}    
-				
-		    	if (nombres_clientes == "")
-		    	{
-			    	
-		    		$("#mensaje_nombres_clientes").text("Introduzca un Nombre");
-		    		$("#mensaje_nombres_clientes").fadeIn("slow"); //Muestra mensaje de error
-		            return false;
-			    }
-		    	else 
-		    	{
-		    		$("#mensaje_nombres_clientes").fadeOut("slow"); //Muestra mensaje de error
-		            
-				}  
-
-		    	if (telefono_clientes == "")
-		    	{
-			    	
-		    		$("#mensaje_telefono_clientes").text("Introduzca un Teléfono");
-		    		$("#mensaje_telefono_clientes").fadeIn("slow"); //Muestra mensaje de error
-		            return false;
-			    }
-		    	else 
-		    	{
-		    		$("#mensaje_telefono_clientes").fadeOut("slow"); //Muestra mensaje de error
-		            
-				} 
-
-		    	if (celular_clientes == "")
-		    	{
-			    	
-		    		$("#mensaje_celular_clientes").text("Introduzca un Celular");
-		    		$("#mensaje_celular_clientes").fadeIn("slow"); //Muestra mensaje de error
-		            return false;
-			    }
-		    	else 
-		    	{
-		    		$("#mensaje_celular_clientes").fadeOut("slow"); //Muestra mensaje de error
-		            
-				}
-
-		    	if (direccion_clientes == "")
-		    	{
-			    	
-		    		$("#mensaje_direccion_clientes").text("Introduzca una Dirección");
-		    		$("#mensaje_direccion_clientes").fadeIn("slow"); //Muestra mensaje de error
-		            return false;
-			    }
-		    	else 
-		    	{
-		    		$("#mensaje_direccion_clientes").fadeOut("slow"); //Muestra mensaje de error
-		            
-				}
-		    	
-		    	
-
-		    					    
-
-			}); 
-
-		    $( "#identificacion_clientes" ).focus(function() {
-				  $("#mensaje_identificacion_clientes").fadeOut("slow");
-			    });
-				
-		        $( "#nombres_clientes" ).focus(function() {
-				  $("#mensaje_nombres_clientes").fadeOut("slow");
-			    });
-
-		        $( "#telefono_clientes" ).focus(function() {
-					  $("#mensaje_telefono_clientes").fadeOut("slow");
-				    });
-		        $( "#celular_clientes" ).focus(function() {
-					  $("#mensaje_celular_clientes").fadeOut("slow");
-				    });
-		        $( "#direccion_clientes" ).focus(function() {
-					  $("#mensaje_direccion_clientes").fadeOut("slow");
-				    });
-				
-		
-				
-		      
-				    
-		}); 
-
-	</script>
-	
     <script>
     $(document).ready(function(){
         $("#marcar_todo").change(function () {
@@ -375,19 +231,22 @@ $(document).ready(function() {
 		 
 		    <h4 style="color:#ec971f;">Asignar Titulos Credito</h4>
             	<hr/>
-		   	<div class="col-xs-5">
+		   	<div class="col-xs-6">
 			  	<p  class="formulario-subtitulo" >Ciudad</p>
 			  	<select name="id_ciudad" id="id_ciudad"  class="form-control" >
+					<option value="0">--Seleccione--</option>
 					<?php foreach($resultCiu as $resCiu) {?>
 						<option value="<?php echo $resCiu->id_ciudad; ?>"  ><?php echo $resCiu->nombre_ciudad; ?> </option>
 			        <?php } ?>
-				</select> 			  
+			        	   
+		   </select> 
+		   <div id="mensaje_id_ciudad" class="errores"></div>			  
 			  </div>
 			  
 			  
 		
 		  
-		    <div class="col-xs-5">
+		    <div class="col-xs-6">
 			  <p  class="formulario-subtitulo" >Abogado(a)</p>
 	            	 <select name="id_usuarioImpulsor" id="id_usuarioImpulsor"  class="form-control">
 						<option value="0"  > -- SIN ESPECIFICAR -- </option>			
@@ -396,15 +255,15 @@ $(document).ready(function() {
 		   		   
 		    </div>
 			  
-			  <div class="col-xs-5" >
-			  <p style="color:#ffffff;" >-----</p>
-			
+			  <div class="row">
+			  <div class="col-xs-12 col-md-12" style="text-align: center; margin-top:20px" >
 			  	<input type="submit" id="Guardar" name="Guardar" value="Guardar" onClick="Ok()" class="btn btn-success"/>
+			  <hr>
 			  </div>
+			  
+			</div>   
 			 
-			<div class="col-xs-1"  style="width: 400px;">
-			<hr>
-			</div>
+		
 			 
 			
 			
@@ -437,9 +296,6 @@ $(document).ready(function() {
 
 		     	<input type="submit" id="buscar" name="buscar"    onclick="this.form.action='<?php echo $helper->url("AsignacionTituloCredito","index"); ?>'" value="buscar" class="btn btn-default"/>
 
-		
-			  	<input type="submit" id="buscar" name="buscar"   onClick="notificacion()" onclick="this.form.action='<?php echo $helper->url("AsignacionTituloCredito","Index"); ?>'" value="buscar" class="btn btn-default"/>
-
 			</div>
 		<div class="col-xs-12" style="margin: 10px;">	
 
@@ -452,7 +308,7 @@ $(document).ready(function() {
         <table class="table table-hover ">
 	         <tr >
 	    		<th style="color:#456789;font-size:80%;"><input type="checkbox" id="marcar_todo" class="checkbox"> </th>
-	    		<th style="color:#456789;font-size:80%;">Id</th>
+	    		<th style="color:#456789;font-size:80%;">Nº Titulo Credito</th>
 	    		<th style="color:#456789;font-size:80%;">Numero de Identifiación</th>
 	    		<th style="color:#456789;font-size:80%;">Nombres Cliente</th>
 	    		<th style="color:#456789;font-size:80%;">Celular Cliente</th>
