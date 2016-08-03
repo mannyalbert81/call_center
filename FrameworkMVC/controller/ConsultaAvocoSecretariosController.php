95b90e38974aaaa37f3a62c16bc5cf3969a3783f
mAYCOL <maycol@masoft.net>
2016-08-03 11:57:06 -0500

    fgdfgdfg

---
+++ FrameworkMVC/controller/ConsultaAvocoSecretariosController.php
@@ -0,0 +1,501 @@
+<?php
+class ConsultaAvocoSecretariosController extends ControladorBase{
+	
+	public function __construct() {
+		parent::__construct();
+	}
+
+	
+	public function consulta_secretarios_avoco(){
+
+		session_start();
+
+		//Creamos el objeto usuario
+		$resultSet="";
+		$avoco_secretarios=new AvocoConocimientoModel();
+		$usuarios = new UsuariosModel();
+		// saber la ciudad del usuario
+		$_id_usuarios= $_SESSION["id_usuarios"]; 
+		
+		$columnas = " usuarios.id_ciudad, 
+					  ciudad.nombre_ciudad, 
+					  usuarios.nombre_usuarios";
+			
+		$tablas   = "public.usuarios, 
+                     public.ciudad";
+			
+		$where    = "ciudad.id_ciudad = usuarios.id_ciudad AND usuarios.id_usuarios = '$_id_usuarios'";
+			
+		$id       = "usuarios.id_ciudad";
+		$resultDatos=$usuarios->getCondiciones($columnas ,$tablas ,$where, $id);
+		
+		
+		// saber los impulsores del secretario
+		$_id_usuarios= $_SESSION["id_usuarios"];
+		
+		$columnas = " asignacion_secretarios_view.id_abogado,
+					  asignacion_secretarios_view.impulsores";
+			
+		$tablas   = "public.asignacion_secretarios_view";
+			
+		$where    = "public.asignacion_secretarios_view.id_secretario = '$_id_usuarios'";
+			
+		$id       = "asignacion_secretarios_view.id_abogado";
+		$resultImpul=$avoco_secretarios->getCondiciones($columnas ,$tablas ,$where, $id);
+		
+		
+		$ciudad = new CiudadModel();
+		$resultCiu = $ciudad->getAll("nombre_ciudad");
+		
+		
+		$usuarios = new UsuariosModel();
+		$resultUsu = $usuarios->getAll("nombre_usuarios");
+		
+
+		$avoco_secretarios=new AvocoConocimientoModel();
+
+
+		if (isset(  $_SESSION['usuario_usuarios']) )
+		{
+			//notificaciones
+			$avoco_secretarios->MostrarNotificaciones($_SESSION['id_usuarios']);
+			
+			$permisos_rol = new PermisosRolesModel();
+			$nombre_controladores = "ConsultaAvocoSecretarios";
+			$id_rol= $_SESSION['id_rol'];
+			$resultPer = $avoco_secretarios->getPermisosVer("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+
+			if (!empty($resultPer))
+			{
+					
+				if(isset($_POST["buscar"])){
+
+					$id_ciudad=$_POST['id_ciudad'];
+					$id_usuarios=$_POST['id_usuarios'];
+					$identificacion=$_POST['identificacion'];
+					$numero_juicio=$_POST['numero_juicio'];
+					$fechadesde=$_POST['fecha_desde'];
+					$fechahasta=$_POST['fecha_hasta'];
+
+					$avoco_secretarios=new AvocoConocimientoModel();
+
+
+					$columnas = "avoco_conocimiento.id_avoco_conocimiento, 
+							  juicios.juicio_referido_titulo_credito, 
+							  clientes.nombres_clientes, 
+							  clientes.identificacion_clientes, 
+							  ciudad.nombre_ciudad, 
+							  asignacion_secretarios_view.secretarios, 
+							  asignacion_secretarios_view.impulsores, 
+							  usuarios.nombre_usuarios, 
+							  avoco_conocimiento.creado";
+
+					$tablas=" public.avoco_conocimiento, 
+							  public.juicios, 
+							  public.ciudad, 
+							  public.asignacion_secretarios_view, 
+							  public.clientes, 
+							  public.usuarios";
+
+					$where="avoco_conocimiento.id_secretario = asignacion_secretarios_view.id_secretario AND
+						  avoco_conocimiento.id_impulsor = asignacion_secretarios_view.id_abogado AND
+						  avoco_conocimiento.secretario_reemplazo = usuarios.id_usuarios AND
+						  juicios.id_juicios = avoco_conocimiento.id_juicios AND
+						  ciudad.id_ciudad = avoco_conocimiento.id_ciudad AND
+						  clientes.id_clientes = juicios.id_clientes AND 
+						  avoco_conocimiento.firma_impulsor='TRUE' AND avoco_conocimiento.firma_secretario='FALSE'";
+
+					$id="avoco_conocimiento.id_avoco_conocimiento";
+						
+						
+					$where_0 = "";
+					$where_1 = "";
+					$where_2 = "";
+					$where_3 = "";
+					$where_4 = "";
+
+
+					if($id_ciudad!=0){$where_0=" AND ciudad.id_ciudad='$id_ciudad'";}
+					
+					if($id_usuarios!=0){$where_1=" AND usuarios.id_usuarios='$id_usuarios'";}
+						
+					if($identificacion!=""){$where_2=" AND clientes.identificacion_clientes='$identificacion'";}
+						
+					if($numero_juicio!=""){$where_3=" AND juicios.juicio_referido_titulo_credito='$numero_juicio'";}
+					
+					if($fechadesde!="" && $fechahasta!=""){$where_4=" AND  avoco_conocimiento.creado BETWEEN '$fechadesde' AND '$fechahasta'";}
+
+                    $where_to  = $where . $where_0 . $where_1 . $where_2. $where_3 . $where_4;
+                    
+                    $resultSet=$avoco_secretarios->getCondiciones($columnas ,$tablas , $where_to, $id);
+
+
+				}
+				
+				if(isset($_POST['firmar']))
+				{
+					$firmas= new FirmasDigitalesModel();
+					$avoco=new AvocoConocimientoModel();
+					$tipo_notificacion = new TipoNotificacionModel();
+					$asignacion_secreatario= new AsignacionSecretariosModel();
+					
+					$ruta="Avoco";
+					$nombrePdf="";
+					
+					$destino = $_SERVER['DOCUMENT_ROOT'].'/documentos/';
+					
+					$array_documento=$_POST['file_firmar'];
+					
+					$permisosFirmar=$permisos_rol->getPermisosFirmarPdfs($_id_usuarios);
+					
+					//para las notificaciones 
+					$_nombre_tipo_notificacion="avoco";					
+					$resul_tipo_notificacion=$tipo_notificacion->getBy("descripcion_notificacion='$_nombre_tipo_notificacion'");						
+					$id_tipo_notificacion=$resul_tipo_notificacion[0]->id_tipo_notificacion;					
+					$descripcion="Avoco Firmado por";
+					$numero_movimiento=0;
+					$id_impulsor="";
+					
+					if($permisosFirmar['estado'])
+					{
+						
+						$id_firma = $permisosFirmar['valor'];
+						
+						
+						foreach ($array_documento as $id )
+						{
+														
+							if(!empty($id))
+							{
+								
+								$id_documento = $id;
+								
+								$resultDocumento=$avoco->getBy("id_avoco_conocimiento='$id'");
+								
+								$nombrePdf=$resultDocumento[0]->nombre_documento;
+								
+								$nombrePdf=$nombrePdf.".pdf";
+								
+								$ruta=$resultDocumento[0]->ruta_documento;
+				
+								$id_rol=$_SESSION['id_rol'];
+								
+								$destino.=$ruta.'/';
+								
+								
+								try {
+									
+									$res=$firmas->FirmarPDFs( $destino, $nombrePdf, $id_firma,$id_rol);
+									
+									$firmas->UpdateBy("firma_secretario='TRUE'", "avoco_conocimiento", "id_avoco_conocimiento='$id_documento'");
+									
+									
+									//dirigir notificacion
+									$usuarioDestino=$resultDocumento[0]->id_usuario_registra_avoco;
+									
+									$result_notificaciones=$firmas->CrearNotificacion($id_tipo_notificacion, $usuarioDestino, $descripcion, $numero_movimiento, $nombrePdf);
+											
+									
+									
+																		
+								} catch (Exception $e) {
+									
+									echo $e->getMessage();
+								}
+								
+							}
+						}
+					}
+				}
+
+
+
+
+				$this->view("ConsultaAvocoSecretarios",array(
+						"resultSet"=>$resultSet,"resultCiu"=>$resultCiu, "resultUsu"=>$resultUsu, "resultDatos"=>$resultDatos, "resultImpul"=>$resultImpul
+							
+				));
+
+
+
+			}
+			else
+			{
+				$this->view("Error",array(
+						"resultado"=>"No tiene Permisos de Acceso a Firmar Avoco Secretarios"
+
+				));
+
+				exit();
+			}
+
+		}
+		else
+		{
+			$this->view("ErrorSesion",array(
+					"resultSet"=>""
+
+			));
+
+		}
+
+	}
+
+
+	
+	public function consulta_secretarios_avoco_firmados(){
+	
+		session_start();
+	
+		//Creamos el objeto usuario
+		$resultSet="";
+		$avoco_secretarios=new AvocoConocimientoModel();
+		$usuarios = new UsuariosModel();
+		// saber la ciudad del usuario
+		$_id_usuarios= $_SESSION["id_usuarios"];
+	
+		$columnas = " usuarios.id_ciudad,
+					  ciudad.nombre_ciudad,
+					  usuarios.nombre_usuarios";
+			
+		$tablas   = "public.usuarios,
+                     public.ciudad";
+			
+		$where    = "ciudad.id_ciudad = usuarios.id_ciudad AND usuarios.id_usuarios = '$_id_usuarios'";
+			
+		$id       = "usuarios.id_ciudad";
+		$resultDatos=$usuarios->getCondiciones($columnas ,$tablas ,$where, $id);
+	
+	
+		// saber los impulsores del secretario
+		$_id_usuarios= $_SESSION["id_usuarios"];
+	
+		$columnas = " asignacion_secretarios_view.id_abogado,
+					  asignacion_secretarios_view.impulsores";
+			
+		$tablas   = "public.asignacion_secretarios_view";
+			
+		$where    = "public.asignacion_secretarios_view.id_secretario = '$_id_usuarios'";
+			
+		$id       = "asignacion_secretarios_view.id_abogado";
+		$resultImpul=$avoco_secretarios->getCondiciones($columnas ,$tablas ,$where, $id);
+	
+	
+		$ciudad = new CiudadModel();
+		$resultCiu = $ciudad->getAll("nombre_ciudad");
+	
+	
+		$usuarios = new UsuariosModel();
+		$resultUsu = $usuarios->getAll("nombre_usuarios");
+	
+	
+	    $avoco_secretarios=new AvocoConocimientoModel();
+	
+	
+		if (isset(  $_SESSION['usuario_usuarios']) )
+		{
+			$permisos_rol = new PermisosRolesModel();
+			$nombre_controladores = "ConsultaAvocoSecretarios";
+			$id_rol= $_SESSION['id_rol'];
+			$resultPer = $avoco_secretarios->getPermisosVer("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
+	
+			if (!empty($resultPer))
+			{
+					
+				if(isset($_POST["buscar"])){
+	
+					$id_ciudad=$_POST['id_ciudad'];
+					$id_usuarios=$_POST['id_usuarios'];
+					$identificacion=$_POST['identificacion'];
+					$numero_juicio=$_POST['numero_juicio'];
+					$fechadesde=$_POST['fecha_desde'];
+					$fechahasta=$_POST['fecha_hasta'];
+	
+					 $avoco_secretarios=new AvocoConocimientoModel();
+	
+	
+					$columnas = "avoco_conocimiento.id_avoco_conocimiento, 
+							  juicios.juicio_referido_titulo_credito, 
+							  clientes.nombres_clientes, 
+							  clientes.identificacion_clientes, 
+							  ciudad.nombre_ciudad, 
+							  asignacion_secretarios_view.secretarios, 
+							  asignacion_secretarios_view.impulsores, 
+							  usuarios.nombre_usuarios, 
+							  avoco_conocimiento.creado";
+	
+					$tablas=" public.avoco_conocimiento, 
+							  public.juicios, 
+							  public.ciudad, 
+							  public.asignacion_secretarios_view, 
+							  public.clientes, 
+							  public.usuarios";
+	
+					$where="avoco_conocimiento.id_secretario = asignacion_secretarios_view.id_secretario AND
+						  avoco_conocimiento.id_impulsor = asignacion_secretarios_view.id_abogado AND
+						  avoco_conocimiento.secretario_reemplazo = usuarios.id_usuarios AND
+						  juicios.id_juicios = avoco_conocimiento.id_juicios AND
+						  ciudad.id_ciudad = avoco_conocimiento.id_ciudad AND
+						  clientes.id_clientes = juicios.id_clientes
+						AND avoco_conocimiento.firma_impulsor='TRUE' AND avoco_conocimiento.firma_secretario ='TRUE'";
+	
+					$id="avoco_conocimiento.id_avoco_conocimiento";
+	
+	
+					$where_0 = "";
+					$where_1 = "";
+					$where_2 = "";
+					$where_3 = "";
+					$where_4 = "";
+	
+	
+					if($id_ciudad!=0){$where_0=" AND ciudad.id_ciudad='$id_ciudad'";}
+						
+					if($id_usuarios!=0){$where_1=" AND usuarios.id_usuarios='$id_usuarios'";}
+	
+					if($identificacion!=""){$where_2=" AND clientes.identificacion_clientes='$identificacion'";}
+	
+					if($numero_juicio!=""){$where_3=" AND juicios.juicio_referido_titulo_credito='$numero_juicio'";}
+						
+					if($fechadesde!="" && $fechahasta!=""){$where_4=" AND  avoco_conocimiento.creado BETWEEN '$fechadesde' AND '$fechahasta'";}
+	
+	
+					$where_to  = $where . $where_0 . $where_1 . $where_2. $where_3 . $where_4;
+	
+	
+					$resultSet=$avoco_secretarios->getCondiciones($columnas ,$tablas , $where_to, $id);
+	
+	         }
+	
+				$this->view("ConsultaAvocoSecretariosFirmados",array(
+						"resultSet"=>$resultSet,"resultCiu"=>$resultCiu, "resultUsu"=>$resultUsu, "resultDatos"=>$resultDatos, "resultImpul"=>$resultImpul
+							
+				));
+	
+	         }
+			else
+			{
+				$this->view("Error",array(
+						"resultado"=>"No tiene Permisos de Acceso a Consulta Avoco Secretarios Firmados"
+	
+				));
+	
+				exit();
+			}
+	
+		}
+		else
+		{
+			$this->view("ErrorSesion",array(
+					"resultSet"=>""
+	
+			));
+	
+		}
+	
+	}
+
+	
+	public function abrirPdf()
+	{
+		$avoco = new AvocoConocimientoModel();
+		
+		if(isset($_GET['id']))
+		{
+			
+			$id_documento = $_GET ['id'];
+			
+			$resultDocumento = $avoco->getBy ( "id_avoco_conocimiento='$id_documento'" );
+			
+			if (! empty ( $resultDocumento )) {
+				
+				$nombrePdf = $resultDocumento [0]->nombre_documento;
+				
+				$nombrePdf .= ".pdf";
+				
+				$ruta = $resultDocumento [0]->ruta_documento;
+				
+				$directorio = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/' . $ruta . '/' . $nombrePdf;
+				
+				header('Content-type: application/pdf');
+				header('Content-Disposition: attachment; filename="'.$directorio.'"');
+				readfile($directorio);
+			}
+		
+		
+		}
+		
+	
+		
+	}
+	
+	public function rechazarPdf()
+	{
+		session_start();
+		
+		$avoco = new AvocoConocimientoModel();
+
+		$firmas= new FirmasDigitalesModel();
+	
+		$tipo_notificacion = new TipoNotificacionModel();
+		$asignacion_secreatario= new AsignacionSecretariosModel();
+	
+			
+		//para las notificaciones
+		$_nombre_tipo_notificacion="rechazado";
+		$resul_tipo_notificacion=$tipo_notificacion->getBy("descripcion_notificacion='$_nombre_tipo_notificacion'");
+		$id_tipo_notificacion=$resul_tipo_notificacion[0]->id_tipo_notificacion;
+		$descripcion="Avoco Rechazo por";
+		$numero_movimiento=0;
+		$id_impulsor="";
+	
+		if(isset($_GET['id']))
+		{
+	
+			$id_documento = $_GET ['id'];
+			$resultDocumento = $avoco->getBy ( "id_avoco_conocimiento='$id_documento'" );
+				
+				
+			if (! empty ( $resultDocumento )) {
+	
+				$nombrePdf = $resultDocumento [0]->nombre_documento;
+	
+				$nombrePdf .= ".pdf";
+	
+				$ruta = $resultDocumento [0]->ruta_documento;
+	
+				$directorio = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/' . $ruta . '/' . $nombrePdf;
+	
+	
+				try {
+						
+					$eliminado=unlink($directorio);
+					$resultDocumento=$documentos->deleteById("id_avoco_conocimiento='$id_documento'");
+					
+					
+					//dirigir notificacion
+					$usuarioDestino=$resultDocumento[0]->id_usuario_registra_avoco;
+						
+					$result_notificaciones=$firmas->CrearNotificacion($id_tipo_notificacion, $usuarioDestino, $descripcion, $numero_movimiento, $nombrePdf);
+										
+					//$this->view("Error", array("resultado"=>"no se elimino el archivo <br>".print_r($result_notificaciones)));
+					//$this->redirect("ConsultaDocumentosSecretarios","consulta_secretarios");
+					
+				} catch (Exception $e)
+				{
+					$this->view("Error", array("resultado"=>"no se elimino el archivo <br>".$e->getMessage()));
+				}
+					
+			}
+	
+	
+		}
+	
+	
+	}
+
+//129;681;19;4;"2016-07-22";"02:11:00";"DSFSD";"FSDFSD";" SDFSD";41;FALSE;FALSE;FALSE;"Providencias1065";"Providencias";"2016-07-21 19:10:34.858-05";""
+
+
+}
+?>
---
+++ FrameworkMVC/view/ConsultaAvocoSecretariosView.php
@@ -0,0 +1,324 @@
+ <!DOCTYPE HTML>
+<html lang="es">
+
+      <head>
+      
+        <meta charset="utf-8"/>
+        <title>Firmar Avoco Secretarios - coactiva 2016</title>
+        
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		  			   
+          <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	      <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		
+		<link rel="stylesheet" href="http://jqueryvalidation.org/files/demo/site-demos.css">
+        <script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>
+        <script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+ 		
+ 		<script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
+		
+		<script>
+		    webshims.setOptions('forms-ext', {types: 'date'});
+			webshims.polyfill('forms forms-ext');
+		</script>
+		
+           <!-- AQUI NOTIFICAIONES -->
+		<script type="text/javascript" src="view/css/lib/alertify.js"></script>
+		<link rel="stylesheet" href="view/css/themes/alertify.core.css" />
+		<link rel="stylesheet" href="view/css/themes/alertify.default.css" />
+		
+		
+		
+		<script>
+
+		function Ok(){
+				alertify.success("Has Pulsado en Reporte"); 
+				return false;
+			}
+			
+			function Borrar(){
+				alertify.success("Has Pulsado en Borrar"); 
+				return false; 
+			}
+
+			function notificacion(){
+				alertify.success("Has Pulsado en Buscar"); 
+				return false; 
+			}
+		</script>
+		
+		
+		
+		<!-- TERMINA NOTIFICAIONES -->
+        
+       <style>
+            input{
+                margin-top:5px;
+                margin-bottom:5px;
+            }
+            .right{
+                float:right;
+            }
+                
+            
+        </style>
+         
+         
+	<script>
+	$(document).ready(function(){
+			$("#fecha_hasta").change(function(){
+				 var startDate = new Date($('#fecha_desde').val());
+
+                 var endDate = new Date($('#fecha_hasta').val());
+
+                 if (startDate > endDate){
+ 
+                    $("#mensaje_fecha_hasta").text("Fecha desde no debe ser mayor ");
+		    		$("#mensaje_fecha_hasta").fadeIn("slow"); //Muestra mensaje de error  
+		    		$("#fecha_hasta").val("");
+
+                        }
+				});
+
+			 $( "#fecha_hasta" ).focus(function() {
+				  $("#mensaje_fecha_hasta").fadeOut("slow");
+			   });
+			});
+        </script>
+        
+  <script>
+		$(document).ready(function(){
+			
+				$("#firmar").click(function(){
+	
+					  var selected = '';  
+			          
+				        $('.marcados').each(function(){
+				            if (this.checked) {
+				                selected +=$(this)+' esta '+$(this).val()+', ';
+				            }
+				        }); 
+	
+				        if (selected != '') {
+				            return true;
+				        }
+				        else{
+				            alert('Seleccione Documento(s) a firmar.');
+				            return false;
+				        }
+	
+				      
+				});
+		});
+   </script>
+
+    </head>
+    <body style="background-color: #d9e3e4;">
+    
+       <?php include("view/modulos/modal.php"); ?>
+       <?php include("view/modulos/head.php"); ?>
+       <?php include("view/modulos/menu.php"); ?>
+       
+       <?php
+       
+       $sel_id_ciudad = "";
+       $sel_id_usuarios = "";
+       $sel_identificacion="";
+       $sel_numero_juicio="";
+      
+       $sel_fecha_desde="";
+       $sel_fecha_hasta="";
+       
+       if($_SERVER['REQUEST_METHOD']=='POST' )
+       {
+       
+       
+       	$sel_id_ciudad = $_POST['id_ciudad'];
+       	$sel_id_usuarios = $_POST['id_usuarios'];
+       	$sel_identificacion=$_POST['identificacion'];
+       	$sel_numero_juicio=$_POST['numero_juicio'];
+      
+       	$sel_fecha_desde=$_POST['fecha_desde'];
+       	$sel_fecha_hasta=$_POST['fecha_hasta'];
+       	 
+       }
+       
+     	?>
+ 
+  
+  <div class="container">
+  
+  <div class="row" style="background-color: #ffffff;">
+  
+       <!-- empieza el form --> 
+       
+      <form action="<?php echo $helper->url("ConsultaAvocoSecretarios","consulta_secretarios_avoco"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-12">
+         
+         <!-- comienxza busqueda  -->
+         <div class="col-lg-12" style="margin-top: 10px">
+         
+       	 <h4 style="color:#ec971f;">Firmar Avoco Secretarios</h4>
+       	 
+       	 
+       	 <div class="panel panel-default">
+  			<div class="panel-body">
+  			
+  			
+		   			
+          <div class="col-xs-2">
+			  	<p  class="formulario-subtitulo" style="" >Ciudad:</p>
+			  	<select name="id_ciudad" id="id_ciudad"  class="form-control" readonly>
+			  		<?php foreach($resultDatos as $res) {?>
+						 <option value="<?php echo $res->id_ciudad; ?>" <?php if($sel_id_ciudad==$res->id_ciudad){echo "selected";}?>  ><?php echo $res->nombre_ciudad; ?> </option>
+			            <?php } ?>
+				</select>
+		 </div>
+		 
+		 <div class="col-xs-2">
+			  	<p  class="formulario-subtitulo" style="" >Impulsores:</p>
+			  	<select name="id_usuarios" id="id_usuarios"  class="form-control" >
+			  		<?php foreach($resultImpul as $res) {?>
+						 <option value="<?php echo $res->id_abogado; ?>"<?php if($sel_id_usuarios==$res->id_abogado){echo "selected";}?>  ><?php echo $res->impulsores; ?> </option>
+			           
+			            <?php } ?>  
+			            
+				</select>
+		 </div>
+		 
+		 <div class="col-xs-2 ">
+			  	<p  class="formulario-subtitulo" >Identificacion:</p>
+			  	<input type="text"  name="identificacion" id="identificacion" value="<?php echo $sel_identificacion;?>" class="form-control"/> 
+			    <div id="mensaje_identificacion" class="errores"></div>
+
+         </div>
+		 
+		  <div class="col-xs-2 ">
+			  	<p  class="formulario-subtitulo" >Nº Juicio:</p>
+			  	<input type="text"  name="numero_juicio" id="numero_juicio" value="<?php echo $sel_numero_juicio;?>" class="form-control"/> 
+			    <div id="mensaje_nombres" class="errores"></div>
+
+         </div>
+          
+         
+         <div class="col-xs-2 ">
+         		<p class="formulario-subtitulo" >Desde:</p>
+			  	<input type="date"  name="fecha_desde" id="fecha_desde" value="<?php echo $sel_fecha_desde;?>" class="form-control "/> 
+			    <div id="mensaje_fecha_desde" class="errores"></div>
+		</div>
+         
+          <div class="col-xs-2 ">
+          		<p class="formulario-subtitulo" >Hasta:</p>
+			  	<input type="date"  name="fecha_hasta" id="fecha_hasta" value="<?php echo $sel_fecha_hasta;?>" class="form-control "/> 
+			    <div id="mensaje_fecha_hasta" class="errores"></div>
+		</div>
+		 
+  			</div>
+  		<div class="col-lg-12" style="text-align: center; margin-bottom: 20px">
+  		
+		 <input type="submit" id="buscar" name="buscar" value="Buscar" onClick="notificacion()" class="btn btn-warning " style="margin-top: 10px;"/> 	
+		 
+		 <?php if(!empty($resultSet))  {?>
+		 <a href="/FrameworkMVC/view/ireports/ContDocumentosGeneralReport.php?id_ciudad=<?php  echo $sel_id_ciudad ?>&identificacion=<?php  echo $sel_identificacion?>&numero_juicio=<?php  echo $sel_numero_juicio?>&id_usuarios=<?php  echo $sel_id_usuarios?>&fecha_desde=<?php  echo $sel_fecha_desde?>&fecha_hasta=<?php  echo $sel_fecha_hasta?>" onclick="window.open(this.href, this.target, ' width=1000, height=800, menubar=no');return false" style="margin-top: 10px;" class="btn btn-success">Reporte</a>
+		 
+		
+		 <input type="submit" value="Firmar" id="firmar" name="firmar" class="btn btn-info" style="margin-top: 10px;"/>
+		         
+		  <?php } else {?>
+		  
+		  <?php } ?>
+		 </div>
+		</div>
+        	
+		 </div>
+		 
+		 
+		 <div class="col-lg-12">
+		 
+	     <div class="col-lg-12">
+		 <div class="col-lg-10"></div>
+		 <div class="col-lg-2">
+		 <span class="form-control"><strong>Registros:</strong><?php if(!empty($resultSet)) echo "  ".count($resultSet);?></span>
+		 </div>
+		 
+		 </div>
+		 
+		 <div class="col-lg-12">
+		 
+		 <section class="" style="height:300px;overflow-y:scroll;">
+        <table class="table table-hover ">
+	         <tr >
+	            <th style="color:#456789;font-size:80%;"></th>
+	    		<th style="color:#456789;font-size:80%;"><b>Id</b></th>
+	    		<th style="color:#456789;font-size:80%;">Nº Juicio Referido</th>
+	    		<th style="color:#456789;font-size:80%;">Cliente</th>
+	    		<th style="color:#456789;font-size:80%;">Identificacion</th>
+	    		<th style="color:#456789;font-size:80%;">Ciudad</th>
+	    		<th style="color:#456789;font-size:80%;">Secretario</th>
+	    		<th style="color:#456789;font-size:80%;">Impulsor</th>
+	    		<th style="color:#456789;font-size:80%;">Secretario Saliente </th>
+	    		<th style="color:#456789;font-size:80%;">Fecha </th>
+	    		
+	    		<th></th>
+	    		<th></th>
+	  		</tr>
+            
+	            <?php if (!empty($resultSet)) {  foreach($resultSet as $res) {?>
+	        		<tr>
+	        		   <td> <input type="checkbox" name="file_firmar[]" id="file_firmar" class="marcados" value="<?php echo $res->id_avoco_conocimiento; ?>"/>      </td>
+	        		 <td style="color:#000000;font-size:80%;"> <?php echo $res->id_avoco_conocimiento; ?></td>
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->juicio_referido_titulo_credito; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombres_clientes; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->identificacion_clientes; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_ciudad; ?></td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->secretarios; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->impulsores; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_usuarios; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->creado; ?>     </td> 
+		            
+		               <td style="color:#000000;font-size:80%;">
+		               <a href="<?php echo $helper->url("ConsultaAvocoSecretarios","abrirPdf"); ?>&id=<?php echo $res->id_avoco_conocimiento; ?>" onclick="window.open(this.href, this.target, ' width=1000, height=800, menubar=no');return false" class="btn btn-success" onClick="Ok()" style="font-size:65%;">-- VER --</a>
+		               <a href="<?php echo $helper->url("ConsultaAvocoSecretarios","rechazarPdf"); ?>&id=<?php echo $res->id_avoco_conocimiento; ?>" class="btn btn-danger" onClick="Ok()" style="font-size:65%;">Eliminar</a>
+		               </td> 
+		    		</tr>
+		        <?php } }else {  ?>
+
+                <tr>
+	                  	<td></td>
+            			<td></td>
+            			<td></td>
+            			<td></td>
+            			<td colspan="5" style="color:#ec971f;font-size:8;" style="text-aling:center";> <?php echo '<span id="snResult">NO EXISTE DATOS PARA ESOS FILTROS</span>' ?></td>
+	       				
+		    	</tr>
+		    		
+		    		<?php }?>
+               
+               
+       	</table>
+       </section>
+		</div>
+		 </div>
+<div class="text-center">	
+  <ul class="pagination">
+  <li><a href="#">&laquo;</a></li>
+  <li><a href="#">1</a></li>
+  <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+  <li><a href="#">&raquo;</a></li>
+</ul>                       
+ </div>
+
+
+       </form>
+     
+      </div>
+     
+  </div>
+      <!-- termina
+       busqueda  -->
+   </body>  
+
+    </html>   
