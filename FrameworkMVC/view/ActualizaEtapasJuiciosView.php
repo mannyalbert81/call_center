34226663245ce9c3259824f875e56dd6624ff540
mAYCOL <maycol@masoft.net>
2016-08-05 14:17:46 -0500

    dfgfdg

---
+++ FrameworkMVC/view/ActualizaEtapasJuiciosView.php
@@ -0,0 +1,309 @@
+ <!DOCTYPE HTML>
+<html lang="es">
+
+      <head>
+      
+        <meta charset="utf-8"/>
+        <title>Actualiza Etapas Juicios - coactiva 2016</title>
+        
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		  			   
+          <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	      <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		
+		<link rel="stylesheet" href="http://jqueryvalidation.org/files/demo/site-demos.css">
+        <script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>
+        <script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+ 		
+ 		<script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
+		
+		<script>
+		    webshims.setOptions('forms-ext', {types: 'date'});
+			webshims.polyfill('forms forms-ext');
+		</script>
+		
+		   <!-- AQUI NOTIFICAIONES -->
+		<script type="text/javascript" src="view/css/lib/alertify.js"></script>
+		<link rel="stylesheet" href="view/css/themes/alertify.core.css" />
+		<link rel="stylesheet" href="view/css/themes/alertify.default.css" />
+		
+		
+		
+		<script>
+
+		function Ok(){
+				alertify.success("Has Pulsado en Reporte"); 
+				return false;
+			}
+			
+			function Borrar(){
+				alertify.success("Has Pulsado en Borrar"); 
+				return false; 
+			}
+
+			function notificacion(){
+				alertify.success("Has Pulsado en Buscar"); 
+				return false; 
+			}
+		</script>
+		
+		
+		
+		<!-- TERMINA NOTIFICAIONES -->
+        
+        
+       <style>
+            input{
+                margin-top:5px;
+                margin-bottom:5px;
+            }
+            .right{
+                float:right;
+            }
+                
+            
+        </style>
+         
+         <script >
+		$(document).ready(function(){
+
+		$("#tipo_gasto").change(function(){
+
+
+           var $valor_tipo_gasto = $("#valor_a_distribuir");
+
+           var id_tipo_gasto = $(this).val();
+
+           $valor_tipo_gasto.empty();
+
+           
+           if(id_tipo_gasto > 0)
+           {
+        	  var datos = {id_tipo_gasto : $(this).val() };
+  
+        	  var resultTipogasto= $.post("<?php echo $helper->url("DistribucionGastos","devuelveTipoGasto"); ?>", datos, function(resultTipo_gasto) {
+        		  }, "json");    		  
+
+        	  resultTipogasto.done(function(resultTipo_gasto ) {
+
+        		  $.each(resultTipo_gasto, function(index, value) {
+        			  $valor_tipo_gasto.val(value.valor_tipo_gasto);
+  			 	     });
+            	  });
+
+           }
+           else
+           {
+        	  
+           }
+
+			});  
+				    
+		});
+
+	</script>
+	<script>
+	$(document).ready(function(){
+			$("#fecha_hasta").change(function(){
+				 var startDate = new Date($('#fecha_desde').val());
+
+                 var endDate = new Date($('#fecha_hasta').val());
+
+                 if (startDate > endDate){
+ 
+                    $("#mensaje_fecha_hasta").text("Fecha desde no debe ser mayor ");
+		    		$("#mensaje_fecha_hasta").fadeIn("slow"); //Muestra mensaje de error  
+		    		$("#fecha_hasta").val("");
+
+                        }
+				});
+
+			 $( "#fecha_hasta" ).focus(function() {
+				  $("#mensaje_fecha_hasta").fadeOut("slow");
+			   });
+			});
+        </script>
+
+    </head>
+    <body style="background-color: #d9e3e4;">
+    
+       <?php include("view/modulos/modal.php"); ?>
+       <?php include("view/modulos/head.php"); ?>
+       <?php include("view/modulos/menu.php"); ?>
+       
+       <?php
+     
+       $sel_id_ciudad = "";
+       $sel_identificacion="";
+       $sel_numero_juicio="";
+       $sel_numero_titulo="";
+       $sel_fecha_desde="";
+       $sel_fecha_hasta="";
+       
+       if($_SERVER['REQUEST_METHOD']=='POST' )
+       {
+       
+       
+       	$sel_id_ciudad = $_POST['id_ciudad'];
+       	$sel_identificacion=$_POST['identificacion'];
+       	$sel_numero_juicio=$_POST['numero_juicio'];
+       	$sel_numero_titulo=$_POST['numero_titulo'];
+       	$sel_fecha_desde=$_POST['fecha_desde'];
+       	$sel_fecha_hasta=$_POST['fecha_hasta'];
+       	 
+       }
+       
+		?>
+ 
+  
+  <div class="container">
+  
+  <div class="row" style="background-color: #ffffff;">
+  
+       <!-- empieza el form --> 
+       
+      <form action="<?php echo $helper->url("Juicio","consulta"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-12">
+         
+         <!-- comienxza busqueda  -->
+         <div class="col-lg-12" style="margin-top: 10px">
+         
+       	 <h4 style="color:#ec971f;">Consulta Juicios</h4>
+       	 
+       	 
+       	 <div class="panel panel-default">
+  			<div class="panel-body">
+  			
+  			
+		   			
+          <div class="col-xs-2">
+			  	<p  class="formulario-subtitulo" style="" >Ciudad:</p>
+			  	<select name="id_ciudad" id="id_ciudad"  class="form-control" readonly>
+			  		<?php foreach($resultDatos as $res) {?>
+						 <option value="<?php echo $res->id_ciudad; ?>" <?php if($sel_id_ciudad==$res->id_ciudad){echo "selected";}?>   ><?php echo $res->nombre_ciudad; ?> </option>
+			            <?php } ?>
+				</select>
+		 </div>
+		 
+		 <div class="col-xs-2 ">
+			  	<p  class="formulario-subtitulo" >Identificacion:</p>
+			  	<input type="text"  name="identificacion" id="identificacion" value="<?php echo $sel_identificacion;?>" class="form-control"/> 
+			    <div id="mensaje_identificacion" class="errores"></div>
+
+         </div>
+		 
+		  <div class="col-xs-2 ">
+			  	<p  class="formulario-subtitulo" >Nº Juicio:</p>
+			  	<input type="text"  name="numero_juicio" id="numero_juicio" value="<?php echo $sel_numero_juicio;?>" class="form-control"/> 
+			    <div id="mensaje_nombres" class="errores"></div>
+
+         </div>
+          <div class="col-xs-2 ">
+			  	<p  class="formulario-subtitulo" >Nº Titulo:</p>
+			  	<input type="text"  name="numero_titulo" id="numero_titulo" value="<?php echo $sel_numero_titulo;?>" class="form-control"/> 
+			    <div id="mensaje_numero_titulo" class="errores"></div>
+
+         </div>
+         
+         <div class="col-xs-2 ">
+         		<p class="formulario-subtitulo" >Desde:</p>
+			  	<input type="date"  name="fecha_desde" id="fecha_desde" value="<?php echo $sel_fecha_desde;?>" class="form-control "/> 
+			    <div id="mensaje_fecha_desde" class="errores"></div>
+		</div>
+         
+          <div class="col-xs-2 ">
+          		<p class="formulario-subtitulo" >Hasta:</p>
+			  	<input type="date"  name="fecha_hasta" id="fecha_hasta" value="<?php echo $sel_fecha_hasta;?>" class="form-control "/> 
+			    <div id="mensaje_fecha_hasta" class="errores"></div>
+		</div>
+		 
+  			</div>
+  		<div class="col-lg-12" style="text-align: center; margin-bottom: 20px">
+		 <input type="submit" id="buscar" name="buscar" value="Buscar" class="btn btn-warning " onClick="notificacion()" style="margin-top: 10px;"/> 	
+		
+		<?php if(!empty($resultSet))  {?>
+		 <a href="/FrameworkMVC/view/ireports/ContJuiciosReport.php?id_ciudad=<?php  echo $sel_id_ciudad ?>&identificacion=<?php  echo $sel_identificacion?>&numero_juicio=<?php  echo $sel_numero_juicio?>&numero_titulo=<?php  echo $sel_numero_titulo?>&fecha_desde=<?php  echo $sel_fecha_desde?>&fecha_hasta=<?php  echo $sel_fecha_hasta?>" onclick="window.open(this.href, this.target, ' width=1000, height=800, menubar=no');return false" style="margin-top: 10px;" class="btn btn-success">Reporte</a>
+		  <?php } else {?>
+		  <?php } ?>
+		 </div>
+		</div>
+        	
+		 </div>
+		 
+		 
+		 <div class="col-lg-12">
+		 
+		 <div class="col-lg-12">
+		 <div class="col-lg-10"></div>
+		 <div class="col-lg-2">
+		 <span class="form-control"><strong>Registros:</strong><?php if(!empty($resultSet)) echo "  ".count($resultSet);?></span>
+		 </div>
+		 </div>
+		 <div class="col-lg-12">
+		 
+		 
+		 <section class="" style="height:300px;overflow-y:scroll;">
+        <table class="table table-hover ">
+	         <tr >
+	            
+	    		<th style="color:#456789;font-size:80%;"><b>Id</b></th>
+	    		<th style="color:#456789;font-size:80%;">Coactivad@</th>
+	    		<th style="color:#456789;font-size:80%;">Identificacion</th>
+	    		<th style="color:#456789;font-size:80%;">Ciudad</th>
+	    		<th style="color:#456789;font-size:80%;">Tipo Persona</th>
+	    		<th style="color:#456789;font-size:80%;">Nº Juicio</th>
+	    		<th style="color:#456789;font-size:80%;">Nº Titulo Credito</th>
+	    		<th style="color:#456789;font-size:80%;">Impulsor</th>
+	    		<th style="color:#456789;font-size:80%;">Secretario</th>
+	    		<th style="color:#456789;font-size:80%;">Etapa</th>
+	    		<th style="color:#456789;font-size:80%;">Tipo Juicio</th>
+	    		<th style="color:#456789;font-size:80%;">Fecha Emision</th>
+	    		<th style="color:#456789;font-size:80%;">Total</th>
+	    		<th></th>
+	    		<th></th>
+	  		</tr>
+            
+	            <?php if (!empty($resultSet)) {  foreach($resultSet as $res) {?>
+	        		<tr>
+	        		
+	        		  
+	                   <td style="color:#000000;font-size:80%;"> <?php echo $res->id_juicios; ?></td>
+	                   <td style="color:#000000;font-size:80%;"> <?php echo $res->nombres_clientes; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->identificacion_clientes; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_ciudad; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_tipo_persona; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->juicio_referido_titulo_credito; ?>     </td> 
+		                <td style="color:#000000;font-size:80%;"> <?php echo $res->id_titulo_credito; ?>     </td>
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->impulsores; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->secretarios; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_etapas; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->nombre_tipo_juicios; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->creado; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;"> <?php echo $res->total; ?>     </td> 
+		               <td style="color:#000000;font-size:80%;">
+		               <a href="/FrameworkMVC/view/ireports/ContJuiciosSubReport.php?id_juicios=<?php echo $res->id_juicios; ?>" onclick="window.open(this.href, this.target, ' width=1000, height=800, menubar=no');return false" class="btn btn-success" onClick="Ok()" style="font-size:80%;">Ver</a>
+		               </td> 
+		    		</tr>
+		        <?php } }  ?>
+           
+       	</table>     
+      </section>
+      
+      </div>
+		 
+		 		 
+		 </div>
+		 
+		
+		
+      
+       </form>
+     
+      </div>
+     
+  </div>
+      <!-- termina
+       busqueda  -->
+   </body>  
+
+    </html>   
